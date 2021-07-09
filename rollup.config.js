179b5c5d4df8627ed5ce41909ca7001c4bae066e
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-09 19:17:09 +0200

    update: remove siroc and use rollup directly * add rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import bundleSize from 'rollup-plugin-bundle-size';
+import replace from '@rollup/plugin-replace';
+import svgr from '@svgr/rollup';
+import typescript from 'rollup-plugin-typescript2';
+import { DEFAULT_EXTENSIONS as DEFAULT_BABEL_EXTENSIONS } from '@babel/core';
+
+import pkg from './package.json';
+
+const isProd = process.env.NODE_ENV === 'production';
+const isTesting = process.env.NODE_ENV === 'testing';
+const processEnv = isProd || isTesting ? 'production' : 'development';
+
+export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injectCSS = true } = {}) => ({
+  input: 'src/index.ts',
+  external: ['react', 'react-dom', (id) => id.includes('@babel/runtime')],
+  onwarn(warning, rollupWarn) {
+    if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+      rollupWarn(warning);
+    }
+  },
+  output: [
+    {
+      file: mainFile,
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named'
+    },
+    {
+      file: moduleFile,
+      format: 'esm',
+      sourcemap: true,
+      exports: 'named'
+    }
+  ],
+  plugins: [
+    replace({
+      __ENV__: JSON.stringify(processEnv),
+      __REACT_FLOW_VERSION__: JSON.stringify(pkg.version),
+      preventAssignment: true
+    }),
+    bundleSize(),
+    postcss({
+      minimize: isProd,
+      inject: injectCSS
+    }),
+    svgr(),
+    resolve(),
+    typescript({
+      clean: true
+    }),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      extensions: [...DEFAULT_BABEL_EXTENSIONS, '.ts', '.tsx'],
+      exclude: 'node_modules/**',
+      babelHelpers: 'inline'
+    })
+  ]
+});
+
+export default isProd && !isTesting
+  ? [
+    baseConfig(),
+    baseConfig({
+      mainFile: 'dist/nocss/ReactFlow-nocss.js',
+      moduleFile: 'dist/nocss/ReactFlow-nocss.esm.js',
+      injectCSS: false
+    })
+  ]
+  : baseConfig();
