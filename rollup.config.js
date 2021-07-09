b43623ce672a5a429c09fca0faa5e563f95cde7e
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-09 22:57:04 +0200

    update: rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
-import bundleSize from 'rollup-plugin-bundle-size';
 import replace from '@rollup/plugin-replace';
-import svgr from '@svgr/rollup';
 import typescript from 'rollup-plugin-typescript2';
 import { DEFAULT_EXTENSIONS as DEFAULT_BABEL_EXTENSIONS } from '@babel/core';
 
@@ -14,40 +12,29 @@ const isProd = process.env.NODE_ENV === 'production';
 const isTesting = process.env.NODE_ENV === 'testing';
 const processEnv = isProd || isTesting ? 'production' : 'development';
 
-export const baseConfig = ({ mainFile = pkg.main, moduleFile = pkg.module, injectCSS = true } = {}) => ({
+export const baseConfig = ({ injectCSS = true } = {}) => ({
   input: 'src/index.ts',
-  external: ['react', 'react-dom', (id) => id.includes('@babel/runtime')],
+  external: ['vue', 'vue-demi', (id) => id.includes('@babel/runtime')],
   onwarn(warning, rollupWarn) {
     if (warning.code !== 'CIRCULAR_DEPENDENCY') {
       rollupWarn(warning);
     }
   },
-  output: [
-    {
-      file: mainFile,
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'named'
-    },
-    {
-      file: moduleFile,
-      format: 'esm',
-      sourcemap: true,
-      exports: 'named'
-    }
-  ],
+  output: {
+    dir: 'dist',
+    sourcemap: true,
+    exports: 'named'
+  },
   plugins: [
     replace({
       __ENV__: JSON.stringify(processEnv),
       __REACT_FLOW_VERSION__: JSON.stringify(pkg.version),
       preventAssignment: true
     }),
-    bundleSize(),
     postcss({
       minimize: isProd,
       inject: injectCSS
     }),
-    svgr(),
     resolve(),
     typescript({
       clean: true
