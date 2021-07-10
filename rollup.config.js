adc22c6212733c4470e2c71d14ca0367662031be
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-10 15:40:23 +0200

    update: more bundle stuff

--- rollup.config.js
+++ rollup.config.js
@@ -82,7 +82,7 @@ function createConfig(format, output, plugins = []) {
   output.sourcemap = !!process.env.SOURCE_MAP;
   output.banner = banner;
   output.externalLiveBindings = false;
-  output.globals = { 'vue-demi': 'VueDemi' };
+  output.globals = { vue: 'Vue', 'vue-demi': 'VueDemi' };
 
   const isProductionBuild = /\.prod\.js$/.test(output.file);
   const isGlobalBuild = format.startsWith('global');
@@ -111,7 +111,7 @@ function createConfig(format, output, plugins = []) {
   // during a single build.
   hasTSChecked = true;
 
-  const external = ['vue-demi', (id) => id.includes('@babel/runtime')];
+  const external = ['vue', 'vue-demi', (id) => id.includes('@babel/runtime')];
 
   const nodePlugins = [resolve(), commonjs({ include: 'node_modules/**' })];
 
