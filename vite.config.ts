150438d08fd12129c6df398eb022fe5660011489
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-10 16:40:50 +0200

    update: no vue-2 compatability (doesn't work with current build) * README.md

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'RevueFlow'
-    },
-    sourcemap: true,
-    rollupOptions: {
-      external: ['vue-demi'],
-      output: {
-        globals: {
-          'vue-demi': 'VueDemi'
-        },
-        dir: 'dist',
-        sourcemap: true,
-        exports: 'named'
-      },
-      inlineDynamicImports: true
-    },
-    commonjsOptions: {
-      include: 'node_modules/**'
-    },
-    polyfillDynamicImport: true
+    outDir: 'build'
   },
   plugins: [
     vue(),
