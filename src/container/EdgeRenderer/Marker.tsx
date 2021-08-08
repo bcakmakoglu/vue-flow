d22187bd7bb2771146baee01f96aa29b9361d609
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-08-08 19:24:22 +0200

    fix: Marker arrowheads not displayed

--- src/container/EdgeRenderer/Marker.tsx
+++ src/container/EdgeRenderer/Marker.tsx
@@ -1,22 +1,30 @@
-import { FunctionalComponent } from 'vue';
+import { defineComponent } from 'vue';
 
 interface MarkerProps {
   id: string;
 }
 
-export default ((props: MarkerProps, { slots }) => {
-  return () => (
-    <marker
-      id={props.id}
-      class="revue-flow__arrowhead"
-      markerWidth="12.5"
-      markerHeight="12.5"
-      viewBox="-10 -10 20 20"
-      orient="auto"
-      refX="0"
-      refY="0"
-    >
-      {slots.default ? slots.default() : ''}
-    </marker>
-  );
-}) as FunctionalComponent<MarkerProps>;
+export default defineComponent({
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props: MarkerProps, { slots }) {
+    return () => (
+      <marker
+        id={props.id}
+        class="revue-flow__arrowhead"
+        markerWidth="12.5"
+        markerHeight="12.5"
+        viewBox="-10 -10 20 20"
+        orient="auto"
+        refX="0"
+        refY="0"
+      >
+        {slots.default ? slots.default() : ''}
+      </marker>
+    );
+  }
+});
