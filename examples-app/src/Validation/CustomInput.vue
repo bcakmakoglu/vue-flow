8de1e94e2fce8e4163ba196915a9fad92ec9bb44
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-06 09:03:57 +0100

    update: remove `examples-app` dir
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- examples-app/src/Validation/CustomInput.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-import { Position, Handle, Connection } from '@braks/vue-flow'
-
-const isValidConnection = (connection: Connection) => connection.target === 'B'
-</script>
-<template>
-  <div>Only connectable with B</div>
-  <Handle type="source" :position="Position.Right" :is-valid-connection="isValidConnection" />
-</template>
