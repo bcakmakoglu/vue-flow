066155b9f74586c9e28f31d9941ea2bf8631c574
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-06 13:56:29 +0100

    feat: copy default theme css file separately to dist
    * to enable overwriting the default theme export it separately
    * add rollup plugin copy for copying the css file
    
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- docs/app.vue
+++ docs/app.vue
@@ -9,7 +9,11 @@
 </template>
 <style>
 @import 'assets/index.css';
+/* these are necessary styles for vue flow */
 @import 'node_modules/@braks/vue-flow/dist/style.css';
+
+/* this contains the default theme, you can skip it if you want to */
+@import 'node_modules/@braks/vue-flow/dist/theme-default.css';
 
 #app {
   height: calc(100% - 92px);
