98d70539510576e2207bb06af91287a972122a93
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-15 20:07:03 +0100

    update(docs): Remove docs dir
    * Docs are in a dedicated repo now
    
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- docs/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <ExamplesSidebar />
-  <div id="vue-flow-examples" class="flex-1">
-    <slot />
-  </div>
-</template>
--- docs/pages/index.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts" setup></script>
-<template>
-  <div class="flex justify-center items-center h-full w-full">
-    <h1 class="text-5xl">Vue Flow</h1>
-  </div>
-</template>
