e1e656ccf27ce2e217738f2b0ec38ed63d058283
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-16 18:59:30 +0100

    update: export default nodes and edges
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as VueFlow } from './container/VueFlow/VueFlow.vue'
 export { default as Handle } from './components/Handle/Handle.vue'
-export { default as EdgeText } from './components/Edges/EdgeText.vue'
+export * from './components/Edges'
+export * from './components/Nodes'
 export { getBezierPath, getSmoothStepPath, getMarkerEnd, getCenter as getEdgeCenter } from './components/Edges/utils'
 
 export {
@@ -21,6 +22,7 @@ export { default as useZoomPanHelper } from './composables/useZoomPanHelper'
 export { default as useStore } from './composables/useStore'
 export { default as useHooks } from './composables/useHooks'
 export { default as useHandle } from './composables/useHandle'
+export { default as useKeyPress } from './composables/useHandle'
 
 export * from './additional-components'
 export * from './types'
