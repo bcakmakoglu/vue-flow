1249d33ffc1507ffdb8c803b8edea1a3da2717a4
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-11-27 15:07:04 +0100

    update(test): add takes options and setState test
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- cypress/integration/1-store/setState.spec.ts
+++ cypress/integration/1-store/setState.spec.ts
@@ -1,25 +1,33 @@
 import { useStore } from '~/composables'
 import { initialState } from '~/utils'
-import { FlowState } from '~/types'
-describe('flow store', () => {
+import { FlowState, FlowStore } from '~/types'
+describe('test store state', () => {
+  let store: FlowStore
+
+  beforeEach(() => (store = useStore()))
+  afterEach(() => store.$dispose())
+
   it('has any initial state', () => {
-    const store = useStore()
     expect(store.$state).to.exist
-    store.$dispose()
   })
 
   it('has default initial state', () => {
-    const store = useStore()
     const initial = initialState()
     Object.keys(store.$state).forEach((state) => {
       const storedState = store[<keyof FlowState>state]
       const initialVal = initial[<keyof FlowState>state]
       expect(JSON.stringify(storedState)).to.eq(JSON.stringify(initialVal))
     })
-    store.$dispose()
   })
 
-  it('has state with options', () => {
+  it('sets state', () => {
+    store.setState({
+      zoomOnScroll: false,
+    })
+    expect(store.zoomOnScroll).to.eq(false)
+  })
+
+  it('takes options', () => {
     const store = useStore({
       zoomOnScroll: false,
     })
