9ba6d81b35620552251657c62b3024db77fe22ff
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-12-12 14:06:48 +0100

    fix(tests): correct tests
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- cypress/integration/2-flow/container.spec.ts
+++ cypress/integration/2-flow/container.spec.ts
@@ -5,8 +5,8 @@ import { isEdge, isNode } from '~/utils'
 
 describe('Render VueFlow', () => {
   const elements: Elements = [
-    { id: '1', type: 'input', data: { label: 'Node 1' }, position: { x: 250, y: 5 } },
-    { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
+    { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+    { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
     { id: 'e1-2', source: '1', target: '2', animated: true },
   ]
 
@@ -36,7 +36,7 @@ describe('Render VueFlow', () => {
   it('renders edges', () => cy.get('.vue-flow__edge').should('have.length', elements.filter(isEdge).length))
 
   it('renders correct node labels', () =>
-    cy.get('.vue-flow__node').each((node) => expect(elements.some((el) => el.data.label === node.text())).to.be.true))
+    cy.get('.vue-flow__node').each((node) => expect(elements.some((el) => el.label === node.text())).to.be.true))
 
   it('renders nodes at correct position', () =>
     cy.get('.vue-flow__node').each((node) => {
