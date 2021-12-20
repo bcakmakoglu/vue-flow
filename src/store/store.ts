2910731c3be1286db90bbc8d7ec424bda9117e49
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-12-20 17:22:47 +0100

    refactor(flow): set elements when initializing store
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import useState from './state'
 import useActions from './actions'
 import useGetters from './getters'
-import { FlowHooksOn, State, Store } from '~/types'
+import { FlowHooksOn, FlowOptions, Store } from '~/types'
 
-export default (preloadedState: State): Store => {
+export default (preloadedState?: FlowOptions): Store => {
   const state = reactive(useState(preloadedState))
   const getters = useGetters(state)
   const actions = useActions(state, getters)
@@ -13,6 +13,11 @@ export default (preloadedState: State): Store => {
     hooksOn[<keyof FlowHooksOn>name] = h.on as any
   })
   actions.setState(state)
+  if (preloadedState) {
+    if (preloadedState.modelValue) actions.setElements(preloadedState.modelValue)
+    if (preloadedState.nodes) actions.setNodes(preloadedState.nodes)
+    if (preloadedState.edges) actions.setEdges(preloadedState.edges)
+  }
 
   return {
     state,
