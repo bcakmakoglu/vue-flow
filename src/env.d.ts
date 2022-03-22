c43b94c93231e43ab80bb55c77061b850338ac0d
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-22 15:22:35 +0100

    chore: merge shims into single file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-svg-loader" />
+
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   // eslint-disable-next-line @typescript-eslint/ban-types
