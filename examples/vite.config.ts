220a3c455c3adfca57d2bc2555ccdb0abd2da481
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-29 20:44:18 +0200

    feat(examples): Add path alias for local dev

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,16 +1,22 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueTypes from 'vite-plugin-vue-type-imports'
 import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve('../package/src'),
+    },
+  },
   plugins: [
     vue(),
     vueTypes(),
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: ['vue', '@vueuse/core'],
-      dts: 'src/auto-imports.d.ts',
+      dts: resolve('src/auto-imports.d.ts'),
     }),
   ],
   optimizeDeps: {
