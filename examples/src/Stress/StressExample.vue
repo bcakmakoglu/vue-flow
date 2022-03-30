6c594a78dd4e56f531761befadd7a07406430475
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-30 20:32:03 +0200

    update(examples): Update stress example syntax

--- examples/src/Stress/StressExample.vue
+++ examples/src/Stress/StressExample.vue
@@ -1,16 +1,21 @@
 <script lang="ts" setup>
-import { VueFlow, FlowInstance } from '@braks/vue-flow'
+import { VueFlow, useVueFlow } from '@braks/vue-flow'
 import { getElements } from './utils'
 
-const instance = ref<FlowInstance>()
-const onLoad = (flowInstance: FlowInstance) => {
-  flowInstance.fitView()
-  instance.value = flowInstance
-  console.log(flowInstance.getNodes())
-}
-
 const { nodes, edges } = getElements(10, 10)
+
+const { getNode, onPaneReady } = useVueFlow({
+  nodes,
+  edges,
+})
+
+onPaneReady((i) => {
+  i.fitView({
+    padding: 0.2,
+  })
+  console.log(i.getElements())
+})
 </script>
 <template>
-  <VueFlow :nodes="nodes" :edges="edges" @pane-ready="onLoad"> </VueFlow>
+  <VueFlow />
 </template>
