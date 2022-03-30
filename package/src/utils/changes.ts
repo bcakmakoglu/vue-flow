d1e8c0f16ee8d0893475c1f9f43fcb496a098129
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-30 17:53:43 +0200

    fix(nodes): use node.extent before trying global nodeExtent

--- package/src/utils/changes.ts
+++ package/src/utils/changes.ts
@@ -150,7 +150,7 @@ export const createPositionChange = (
 
   if (diff) {
     const nextPosition = { x: node.position.x + diff.x, y: node.position.y + diff.y }
-    let currentExtent = nodeExtent || node.extent
+    let currentExtent = node.extent === 'parent' ? nodeExtent : node.extent
 
     if (node.extent === 'parent' && parent && node.dimensions.width && node.dimensions.height) {
       currentExtent =
