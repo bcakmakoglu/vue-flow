36e32996810b9438ce47603cd58a8236c92ffcc7
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-30 17:16:16 +0200

    update(docs): Add windicss
    * remove unused files

---
+++ docs/.vitepress/components/home/nodes/Box.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { Handle, Position } from '@braks/vue-flow'
+
+const emit = defineEmits(['next'])
+</script>
+<template>
+  <div class="px-4 py-2 bg-white shadow-lg rounded-md border-2 border-solid border-black">
+    <slot />
+    <Handle id="a" type="source" :position="Position.Bottom" />
+    <Handle id="b" type="source" :position="Position.Right" />
+  </div>
+</template>
---
+++ docs/.vitepress/components/home/nodes/Output.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { Handle, NodeProps, Position } from '@braks/vue-flow'
+interface RBGOutputNodeProps extends NodeProps {
+  rgb: string
+}
+const props = defineProps<RBGOutputNodeProps>()
+const emit = defineEmits(['next'])
+const next = () => emit('next')
+</script>
+<template>
+  <div :style="{ backgroundColor: props.rgb }" class="rgb-output-node" @click="next">
+    <div class="text-md uppercase">{{ props.rgb }}</div>
+    Click me to continue.
+    <Handle type="target" :position="Position.Left" />
+  </div>
+</template>
+<style>
+.rgb-output-node {
+  padding: 9px;
+  border-radius: 25px;
+  text-align: left;
+  color: white;
+}
+</style>
