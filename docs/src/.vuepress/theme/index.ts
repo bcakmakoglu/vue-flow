07c01039ae8655fc6903aae0a981c4cfbc5bc979
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-01 14:28:56 +0200

    chore(docs): preprocess css

--- docs/src/.vuepress/theme/index.ts
+++ docs/src/.vuepress/theme/index.ts
@@ -1,4 +1,5 @@
 import { resolve } from 'path'
+import { Theme } from 'vuepress'
 
 export default {
   name: 'vuepress-theme-local',
@@ -6,4 +7,4 @@ export default {
   layouts: {
     Layout: resolve(__dirname, 'layouts/default.vue'),
   },
-}
+} as Theme
