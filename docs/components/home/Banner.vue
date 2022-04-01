c560a312b8409f08ac2ae27342f4f0cb3bb0cc5a
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-01 18:53:37 +0200

    chore(docs): Banner size & gap

--- docs/components/home/Banner.vue
+++ docs/components/home/Banner.vue
@@ -5,19 +5,15 @@ import Download from '~icons/carbon/download'
 </script>
 <template>
   <div class="w-full bg-black text-white border-b-1 border-white">
-    <div class="max-w-9/12 md:max-w-3/4 lg:max-w-4xl m-auto py-4 md:py-12">
-      <div class="grid md:grid-cols-3 gap-3 md:gap-0 text-center <md:divide-y md:divide-x divide-blue-200">
+    <div class="max-w-full md:max-w-11/12 m-auto py-4 md:py-12">
+      <div class="grid md:grid-cols-3 gap-3 text-center <md:divide-y md:divide-x divide-blue-200">
         <div class="grid grid-rows-auto gap-2 py-4 md:py-0">
           <div class="text-gray-400 font-semibold text-lg">Stargazers</div>
-          <div class="font-bold text-3xl flex gap-2 items-center justify-center">
-            <Star /> {{ data.stargazers_count }}
-          </div>
+          <div class="font-bold text-3xl flex gap-2 items-center justify-center"><Star /> {{ data.stargazers_count }}</div>
         </div>
         <div class="grid grid-rows-2 gap-2 py-4 md:py-0">
           <div class="text-gray-400 font-semibold text-lg">Downloads (last month)</div>
-          <div class="font-bold text-3xl flex gap-2 items-center justify-center">
-            <Download /> {{ data.downloads }}
-          </div>
+          <div class="font-bold text-3xl flex gap-2 items-center justify-center"><Download /> {{ data.downloads }}</div>
         </div>
         <div class="grid grid-rows-2 gap-2 py-4 md:py-0">
           <div class="text-gray-400 font-semibold text-lg">License</div>
