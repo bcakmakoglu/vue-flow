6d7b5daadd42d2aa41229c711dadffd78e65defa
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-04 20:14:37 +0200

    docs: Add favicons

--- docs/src/.vuepress/head.ts
+++ docs/src/.vuepress/head.ts
@@ -119,4 +119,13 @@ export default [
       href: meta.img,
     },
   ],
+  ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/android-chrome-192x192.png' }],
+  ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/android-chrome-512x512.png' }],
+  ['link', { rel: 'manifest', href: '/favicons/site.webmanifest' }],
+  ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
+  ['meta', { name: 'msapplication-TileColor', content: '#10b981' }],
+  ['meta', { name: 'theme-color', content: '#10b981' }],
 ]
