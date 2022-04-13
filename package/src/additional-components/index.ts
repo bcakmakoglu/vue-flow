2eacdc9830d36d830df4cc0d9c1f9a1550c3cf54
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-13 21:57:57 +0200

    feat: Extend minimap node type

--- package/src/additional-components/index.ts
+++ package/src/additional-components/index.ts
@@ -1,4 +1,3 @@
 export { default as MiniMap } from './MiniMap/MiniMap.vue'
-export { default as MiniMapNode } from './MiniMap/MiniMapNode.vue'
 export { default as Controls } from './Controls/Controls.vue'
 export { default as Background } from './Background/Background.vue'
