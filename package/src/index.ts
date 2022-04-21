d5eb3c9c3d507188b09021903b9636a9bd7c43e9
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-22 01:02:14 +0200

    chore: shorten exports

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,11 +1,8 @@
 export { default as VueFlow } from './container/VueFlow/VueFlow.vue'
+
 export { default as Handle } from './components/Handle/Handle.vue'
-export { default as EdgeText } from './components/Edges/EdgeText.vue'
-export { default as StraightEdge } from './components/Edges/StraightEdge'
-export { default as StepEdge } from './components/Edges/StepEdge'
-export { default as BezierEdge } from './components/Edges/BezierEdge'
-export { default as SimpleBezierEdge } from './components/Edges/SimpleBezierEdge'
-export { default as SmoothStepEdge } from './components/Edges/SmoothStepEdge'
+
+export { StraightEdge, StepEdge, BezierEdge, SimpleBezierEdge, SmoothStepEdge, EdgeText } from './components/Edges'
 
 export {
   getBezierPath,
@@ -38,11 +35,15 @@ export {
 export { applyChanges, applyEdgeChanges, applyNodeChanges } from './utils/changes'
 
 export { defaultEdgeTypes, defaultNodeTypes } from './store'
+
 export { VueFlow as VueFlowInjection, NodeId as NodeIdInjection } from './context'
 
 export { default as useZoomPanHelper } from './composables/useZoomPanHelper'
+
 export { default as useVueFlow } from './composables/useVueFlow'
+
 export { default as useHandle } from './composables/useHandle'
 
 export * from './additional-components'
+
 export * from './types'
