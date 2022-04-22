bb30db7c08708672e7981736c505a1f27ea3a688
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-04-22 15:02:34 +0200

    examples: add initial class to basic

--- examples/src/Basic/Basic.vue
+++ examples/src/Basic/Basic.vue
@@ -2,10 +2,10 @@
 import { VueFlow, MiniMap, Controls, Background, isNode, useVueFlow, Elements } from '@braks/vue-flow'
 
 const elements = ref<Elements>([
-  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
-  { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
-  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
-  { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 }, class: 'light' },
+  { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, class: 'light' },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 }, class: 'light' },
+  { id: '4', label: 'Node 4', position: { x: 400, y: 200 }, class: 'light' },
   { id: 'e1-2', source: '1', target: '2', animated: true },
   { id: 'e1-3', source: '1', target: '3' },
 ])
@@ -14,6 +14,7 @@ const { onPaneReady, onNodeDragStop, onConnect, instance, addEdges } = useVueFlo
   minZoom: 0.2,
   maxZoom: 4,
 })
+
 onPaneReady(({ fitView }) => {
   fitView()
 })
