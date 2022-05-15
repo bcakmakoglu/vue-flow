967e68da16f8b97cccffa78892423f3061a9aac9
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-05-15 17:56:04 +0200

    feat: Add resize-rotate-node pkg

---
+++ packages/resize-rotate-node/vite.config.ts
@@ -0,0 +1,47 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+    },
+    dedupe: ['vue'],
+    extensions: ['.ts', '.vue'],
+  },
+  build: {
+    minify: 'esbuild',
+    emptyOutDir: false,
+    lib: {
+      formats: ['es', 'cjs', 'iife'],
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vueFlowResizeRotateNode',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+  ],
+  optimizeDeps: {
+    include: ['@braks/vue-flow', 'vue', '@vueuse/core'],
+  },
+})
