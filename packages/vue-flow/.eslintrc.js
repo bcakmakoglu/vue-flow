c36d30748243ee55e48ee0370e4ba6d6a6544038
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-05-15 16:59:48 +0200

    refactor(pkg): Move vue-flow into separate pkg directory

--- packages/vue-flow/.eslintrc.js
+++ packages/vue-flow/.eslintrc.js
@@ -3,6 +3,6 @@ module.exports = {
     'no-use-before-define': 0,
     'vue/no-setup-props-destructure': 0,
   },
-  extends: ['../.eslintrc.js'],
+  extends: ['../../.eslintrc.js'],
   ignorePatterns: ['!**/*'],
 }
