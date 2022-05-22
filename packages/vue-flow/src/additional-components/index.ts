0702fb80f4db44beb9d68670e99a3028aaacbb0f
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-05-22 18:18:37 +0200

    feat(vue-flow,components): export `ControlButton.vue`

--- packages/vue-flow/src/additional-components/index.ts
+++ packages/vue-flow/src/additional-components/index.ts
@@ -1,3 +1,4 @@
 export { default as MiniMap } from './MiniMap/MiniMap.vue'
 export { default as Controls } from './Controls/Controls.vue'
+export { default as ControlButton } from './Controls/ControlButton.vue'
 export { default as Background } from './Background/Background.vue'
