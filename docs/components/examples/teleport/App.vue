12246d5879e0f8a7dbb3cbd5a8aec8ae3eb52ce8
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-05-27 01:05:02 +0200

    docs: add teleportable example

--- docs/components/examples/teleport/App.vue
+++ docs/components/examples/teleport/App.vue
@@ -1,23 +1,30 @@
 <script setup>
 import { VueFlow, useVueFlow } from '@braks/vue-flow'
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import Sidebar from './Sidebar.vue'
+import TeleportableNode from './TeleportableNode.vue'
 
 const elements = ref([
   {
     id: '1',
     label: 'Click to teleport',
+    type: 'teleportable',
     position: { x: 125, y: 0 },
+    data: {},
   },
   {
     id: '2',
     label: 'Click to teleport',
+    type: 'teleportable',
     position: { x: 250, y: 100 },
+    data: {},
   },
   {
     id: '3',
     label: 'Click to teleport',
+    type: 'teleportable',
     position: { x: 0, y: 100 },
+    data: {},
   },
   {
     id: 'e1-2',
@@ -25,43 +32,15 @@ const elements = ref([
     target: '2',
   },
 ])
-
-const { onNodeClick } = useVueFlow()
-
-const lastPositions = {}
-
-onNodeClick(({ node, connectedEdges }) => {
-  /**
-   * specify a selector to teleport to
-   * beware that teleports only work after viewpane is ready
-   *
-   * teleported elements still behave like they're at their position before,
-   * i.e. if they emit events, they will still emit them up their regular tree
-   */
-  const target = node.teleport ? null : '#port'
-
-  // teleport to target or disable teleport
-  node.teleport = target
-
-  // hide connected edges when teleporting
-  connectedEdges.forEach((edge) => (edge.hidden = !!target))
-
-  if (!target) {
-    // apply last position (so the transformation gets applied)
-    node.position = lastPositions[node.id]
-  } else {
-    // save last position
-    lastPositions[node.id] = Object.assign({}, { ...node.position })
-
-    // remove current position (so the transformation gets removed)
-    node.position = { x: 0, y: 0 }
-  }
-})
 </script>
 
 <template>
   <div class="teleportflow">
-    <VueFlow v-model="elements" :fit-view-on-init="true" />
+    <VueFlow v-model="elements" :fit-view-on-init="true">
+      <template #node-teleportable="props">
+        <TeleportableNode :id="props.id" />
+      </template>
+    </VueFlow>
     <Sidebar />
   </div>
 </template>
