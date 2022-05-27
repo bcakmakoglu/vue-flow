0dcc5f477b871230cd636a10d98c79aa234b3ce3
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-05-27 13:48:46 +0200

    docs: remove unused edges from nested example

--- docs/components/examples/nested/App.vue
+++ docs/components/examples/nested/App.vue
@@ -58,8 +58,6 @@ const { onConnect, nodes, edges, addEdges, addNodes } = useVueFlow({
     { id: 'e1-4', source: '1', target: '4' },
     { id: 'e1-4c', source: '1', target: '4c' },
     { id: 'e2a-4a', source: '2a', target: '4a' },
-    { id: 'e3-4', source: '3', target: '4' },
-    { id: 'e3-4b', source: '3', target: '4b' },
     { id: 'e4a-4b1', source: '4a', target: '4b1' },
     { id: 'e4a-4b2', source: '4a', target: '4b2' },
     { id: 'e4b1-4b2', source: '4b1', target: '4b2' },
