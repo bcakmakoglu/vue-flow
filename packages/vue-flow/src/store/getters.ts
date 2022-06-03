0b21708b582bedd14bf3ffe3c394766811b2a4c2
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-06-03 20:52:15 +0200

    fix(nodes): `getNode` not returning proper node element
    # What's changed?
    
    * use `.find` instead of computed index

--- packages/vue-flow/src/store/getters.ts
+++ packages/vue-flow/src/store/getters.ts
@@ -3,10 +3,8 @@ import type { ComputedGetters, GraphEdge, GraphNode, State } from '~/types'
 import { getNodesInside, isEdgeVisible } from '~/utils'
 
 export default (state: State): ComputedGetters => {
-  const nodeIds = computed(() => state.nodes.map((n) => n.id))
-  const edgeIds = computed(() => state.edges.map((e) => e.id))
-  const getNode: ComputedGetters['getNode'] = computed(() => (id: string) => state.nodes[nodeIds.value.indexOf(id)])
-  const getEdge: ComputedGetters['getEdge'] = computed(() => (id: string) => state.edges[edgeIds.value.indexOf(id)])
+  const getNode: ComputedGetters['getNode'] = computed(() => (id: string) => state.nodes.find((node) => node.id === id))
+  const getEdge: ComputedGetters['getEdge'] = computed(() => (id: string) => state.edges.find((edge) => edge.id === id))
 
   const getEdgeTypes = computed(() => {
     const edgeTypes: Record<string, any> = {
