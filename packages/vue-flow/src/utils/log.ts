0e4618064b3bc884d6725bb8621b5020f647b5be
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-06-06 19:32:13 +0200

    chore: rebase on dev

--- packages/vue-flow/src/utils/log.ts
+++ packages/vue-flow/src/utils/log.ts
@@ -1,5 +1,5 @@
 export const warn = (message: string, ...args: any[]) => {
-  if (import.meta.env.DEV || process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== 'production') {
     console.warn(`[Vue Flow]: ${message}`, ...args)
   }
 }
