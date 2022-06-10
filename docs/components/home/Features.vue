733b76fe7ee200dc2817196010f76ea013fa9817
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-06-10 17:13:31 +0200

    docs: remove unnecessary div

--- docs/components/home/Features.vue
+++ docs/components/home/Features.vue
@@ -25,13 +25,11 @@ watchDebounced([breakpoints.sm, breakpoints.md, breakpoints.lg, breakpoints.xl, 
     <div
       class="flex flex-col divide-y divide-gray-500 md:divide-y-0 gap-12 md:gap-24 lg:gap-36 max-w-9/12 md:max-w-11/12 lg:max-w-9/12 m-auto py-12 md:py-24 text-center md:text-left"
     >
-      <div ref="basic">
-        <XyzTransition appear-visible xyz="fade down ease-out-back">
-          <div class="flex flex-col md:flex-row gap-12 md:gap-24">
-            <Basic @pane="onLoad" />
-          </div>
-        </XyzTransition>
-      </div>
+      <XyzTransition appear-visible xyz="fade down ease-out-back">
+        <div class="flex flex-col md:flex-row gap-12 md:gap-24">
+          <Basic @pane="onLoad" />
+        </div>
+      </XyzTransition>
 
       <XyzTransition appear-visible xyz="fade down ease-out-back">
         <div class="flex flex-col-reverse md:flex-row flex-unwrap gap-12 md:gap-24">
