0adacd0c0fee73f382c63a41f4235eeef9fbcbdd
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-06-16 09:38:00 +0200

    examples: fix import path

--- examples/vite/src/CustomNode/CustomNode.vue
+++ examples/vite/src/CustomNode/CustomNode.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { Elements, Node, SnapGrid } from '@braks/vue-flow'
-import { ConnectionMode, Controls, MiniMap, Position, VueFlow, isEdge, useVueFlow } from '@braks/vue-flow/src/index'
+import { ConnectionMode, Controls, MiniMap, Position, VueFlow, isEdge, useVueFlow } from '@braks/vue-flow'
 import ColorSelectorNode from './ColorSelectorNode.vue'
 
 const elements = ref<Elements>([])
