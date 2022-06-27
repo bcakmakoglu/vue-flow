e3792b56711b8ec9cbb094cedd772aec14ef63c3
bcakmakoglu <78412429+bcakmakoglu@users.noreply.github.com>
2022-06-27 22:32:04 +0200

    chore(docs): update meta title & description

--- docs/src/.vuepress/head.ts
+++ docs/src/.vuepress/head.ts
@@ -1,6 +1,7 @@
-const meta = {
-  title: '🌊 Vue Flow',
-  description: 'The customizable Vue 3 component bringing interactivity to flowcharts and graphs.',
+export const meta = {
+  title: '🌊 Vue Flow - The customizable Vue3 Flowchart Library',
+  description: `Bring interactivity to your flowcharts and graphs with Vue Flow. It's a highly customizable Vue3 Flowchart library that can be used to visualize your ideas with Vue3 Flowchart. Features seamless zoom & pan and a ton of more features!`,
+  keywords: 'vue, flowchart, graph, vue3, vuejs, vite, nuxt3, nuxtjs, quasar',
   img: 'https://images.prismic.io/bcakmakoglu/8fbdad18-3cd4-46a9-83cf-dbd9fbf60484_vue-flow.png?auto=compress,format',
   url: 'https://vueflow.dev/',
 }
