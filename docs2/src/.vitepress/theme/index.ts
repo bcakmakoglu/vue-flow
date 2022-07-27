a15450e14a65a15b78fe49e9dc9b84b7953f7511
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-28 00:41:36 +0200

    docs: update layout file

--- docs2/src/.vitepress/theme/index.ts
+++ docs2/src/.vitepress/theme/index.ts
@@ -1,10 +1,16 @@
 import type { Theme } from 'vitepress'
 import VueAnimXyz from '@animxyz/vue3'
 import DefaultTheme from 'vitepress/theme'
-import '../../assets/index.css'
+import Layout from './layouts/default.vue'
+import 'virtual:windi.css'
+import '@animxyz/core'
+import '@braks/vue-flow/dist/style.css'
+import '@braks/vue-flow/dist/theme-default.css'
+import './../../assets/index.css'
 
 const CustomTheme = {
   ...DefaultTheme,
+  Layout,
   enhanceApp: ({ app }) => {
     app.use(VueAnimXyz)
   },
