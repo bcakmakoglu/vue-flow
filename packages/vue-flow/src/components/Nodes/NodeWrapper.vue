f42450aec8c08b2f6e05b14c70c86953044bd499
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-08-08 13:14:15 +0200

    fix(nodes): use flush timing `post` in NodeWrapper.vue

--- packages/vue-flow/src/components/Nodes/NodeWrapper.vue
+++ packages/vue-flow/src/components/Nodes/NodeWrapper.vue
@@ -127,7 +127,7 @@ onMounted(() => {
           target: getHandleBounds('.target', nodeElement.value, viewport.zoom),
         }
       },
-      { immediate: true },
+      { immediate: true, flush: 'post' },
     )
   })
 })
