232d00256ad25383e9e94c0804efdab5a489ac10
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-06 18:22:43 +0200

    feat(nodes): add nodeRef injection

--- packages/vue-flow/src/context/index.ts
+++ packages/vue-flow/src/context/index.ts
@@ -1,6 +1,7 @@
-import type { InjectionKey, Slots as TSlots } from 'vue'
+import type { InjectionKey, Ref, Slots as TSlots } from 'vue'
 import type { VueFlowStore } from '~/types'
 
 export const VueFlow: InjectionKey<VueFlowStore> = Symbol('vueFlow')
 export const NodeId: InjectionKey<string> = Symbol('nodeId')
+export const NodeRef: InjectionKey<Ref<HTMLDivElement>> = Symbol('nodeRef')
 export const Slots: InjectionKey<TSlots> = Symbol('slots')
