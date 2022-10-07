cc4a007254d4eeba394c522eceb38cdf32e74175
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 18:38:51 +0200

    feat(tests): add selected elements action tests

---
+++ e2e/cypress/component/1-store/actions/edges/addSelectedEdges.cy.ts
@@ -0,0 +1,31 @@
+import { useVueFlow } from '@braks/vue-flow'
+import { getElements } from '../../../../utils'
+
+const { nodes, edges } = getElements()
+
+describe('Store Action: `addSelectedEdges`', () => {
+  const store = useVueFlow({ id: 'test' })
+  let randomNumber: number
+
+  beforeEach(() => {
+    cy.vueFlow({
+      nodes,
+      edges,
+    })
+  })
+
+  beforeEach(() => {
+    randomNumber = Math.floor(Math.random() * edges.length)
+    store.addSelectedEdges(Array.from({ length: randomNumber }, (_, i) => store.edges.value[i]))
+  })
+
+  it('adds selected edges to store', () => {
+    expect(store.getSelectedEdges.value).to.have.length(randomNumber)
+  })
+
+  it('adds `selected` class to edges', () => {
+    cy.get('.vue-flow__edge').then((edges) => {
+      expect(edges[randomNumber - 1]).to.have.class('selected')
+    })
+  })
+})
---
+++ e2e/cypress/component/1-store/actions/nodes/addSelectedNodes.cy.ts
@@ -0,0 +1,31 @@
+import { useVueFlow } from '@braks/vue-flow'
+import { getElements } from '../../../../utils'
+
+const { nodes, edges } = getElements()
+
+describe('Store Action: `addSelectedNodes`', () => {
+  const store = useVueFlow({ id: 'test' })
+  let randomNumber: number
+
+  beforeEach(() => {
+    cy.vueFlow({
+      nodes,
+      edges,
+    })
+  })
+
+  beforeEach(() => {
+    randomNumber = Math.floor(Math.random() * nodes.length)
+    store.addSelectedNodes(Array.from({ length: randomNumber }, (_, i) => store.nodes.value[i]))
+  })
+
+  it('adds selected nodes to store', () => {
+    expect(store.getSelectedNodes.value).to.have.length(randomNumber)
+  })
+
+  it('adds `selected` class to nodes', () => {
+    cy.get('.vue-flow__node').then((nodes) => {
+      expect(nodes[randomNumber - 1]).to.have.class('selected')
+    })
+  })
+})
