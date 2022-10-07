fe756813c37672a04e067c4201280c056ebd85d5
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 20:06:18 +0200

    feat(tests): add update edge test

---
+++ e2e/cypress/component/1-store/actions/edges/updateEdge.cy.ts
@@ -0,0 +1,34 @@
+import { useVueFlow } from '@braks/vue-flow'
+import { getElements } from '../../../../utils'
+
+const { nodes, edges } = getElements(2, 2)
+
+describe('Store Action: `updateEdge`', () => {
+  const store = useVueFlow({ id: 'test' })
+  let randomIndex: number
+
+  beforeEach(() => {
+    cy.vueFlow({
+      nodes,
+      edges,
+    })
+  })
+
+  beforeEach(() => {
+    randomIndex = Math.floor(Math.random() * edges.length)
+  })
+
+  it('updates edge', () => {
+    store.updateEdge(store.edges.value[randomIndex], {
+      sourceHandle: null,
+      targetHandle: null,
+      source: nodes[0].id,
+      target: nodes[1].id,
+    })
+
+    const storedEdge = store.edges.value[randomIndex]
+
+    expect(storedEdge.source).to.equal(nodes[0].id)
+    expect(storedEdge.target).to.equal(nodes[1].id)
+  })
+})
