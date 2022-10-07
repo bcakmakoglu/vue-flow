8695e2b48e07830082505421f8d684202b22d3a0
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 19:43:04 +0200

    feat(tests): add remove selected elements action tests

---
+++ e2e/cypress/component/1-store/actions/elements/removeSelectedElements.cy.ts
@@ -0,0 +1,48 @@
+import type { GraphEdge, GraphNode } from '@braks/vue-flow'
+import { isNode, useVueFlow } from '@braks/vue-flow'
+import { getElements } from '../../../../utils'
+
+const { nodes, edges } = getElements()
+
+describe('Store Action: `removeSelectedElements`', () => {
+  const store = useVueFlow({ id: 'test' })
+  let randomNumber: number
+  let randomNumber2: number
+
+  beforeEach(() => {
+    cy.vueFlow({
+      nodes,
+      edges,
+    })
+  })
+
+  it('removes passed elements from selected elements in store', () => {
+    randomNumber = Math.floor(Math.random() * [...nodes, ...edges].length)
+    randomNumber2 = Math.floor(Math.random() * randomNumber)
+    store.addSelectedElements(Array.from({ length: randomNumber }, (_, i) => store.getElements.value[i]))
+    store.removeSelectedElements(
+      Array.from({ length: randomNumber2 }, (_, i) => store.getElements.value[i]).reduce(
+        (acc, curr) => {
+          if (isNode(curr)) {
+            acc.nodes.push(curr)
+          } else {
+            acc.edges.push(curr)
+          }
+
+          return acc
+        },
+        { nodes: [] as GraphNode[], edges: [] as GraphEdge[] },
+      ),
+    )
+
+    expect(store.getSelectedElements.value).to.have.length(randomNumber2)
+  })
+
+  it('resets all selected elements in store when no argument is passed', () => {
+    randomNumber = Math.floor(Math.random() * [...nodes, ...edges].length)
+    store.addSelectedElements(Array.from({ length: randomNumber }, (_, i) => store.getElements.value[i]))
+    store.removeSelectedElements()
+
+    expect(store.getSelectedElements.value).to.have.length(0)
+  })
+})
