8fa25180ae62479b074fdde485299af10a2e419a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 18:38:51 +0200

    feat(tests): add selected elements action tests

---
+++ e2e/cypress/component/1-store/actions/elements/addSelectedElements.cy.ts
@@ -0,0 +1,25 @@
+import { useVueFlow } from '@braks/vue-flow'
+import { getElements } from '../../../../utils'
+
+const { nodes, edges } = getElements()
+
+describe('Store Action: `addSelectedElements`', () => {
+  const store = useVueFlow({ id: 'test' })
+  let randomNumber: number
+
+  beforeEach(() => {
+    cy.vueFlow({
+      nodes,
+      edges,
+    })
+  })
+
+  beforeEach(() => {
+    randomNumber = Math.floor(Math.random() * [...nodes, ...edges].length)
+    store.addSelectedElements(Array.from({ length: randomNumber }, (_, i) => store.getElements.value[i]))
+  })
+
+  it('adds selected elements to store', () => {
+    expect(store.getSelectedElements.value).to.have.length(randomNumber)
+  })
+})
