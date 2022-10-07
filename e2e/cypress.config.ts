e81bea1637f7394e47acb776070c61729c702a3d
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 18:17:53 +0200

    feat(tests): add default command timeout of 1000

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   retries: 2,
+  defaultCommandTimeout: 1000,
   component: {
     devServer: {
       framework: 'vue',
