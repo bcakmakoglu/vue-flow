124fcb355c7757aecfb8322677f27111644cef0a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-07 18:47:39 +0200

    chore(tests): update test vite conf

--- e2e/vite.config.ts
+++ e2e/vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '.',
+  server: {
+    fs: {
+      strict: false,
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
+    },
+  },
 })
