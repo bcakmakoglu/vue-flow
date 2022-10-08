26b251886ee3edb92cebc779f4efddef2e70dbad
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-08 14:37:01 +0200

    tests: add additional components tests

---
+++ e2e/cypress/component/3-additional-components/background/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Background, VueFlow } from '@braks/vue-flow'
+</script>
+
+<template>
+  <VueFlow>
+    <Background />
+  </VueFlow>
+</template>
---
+++ e2e/cypress/component/3-additional-components/minimap/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { MiniMap, VueFlow } from '@braks/vue-flow'
+
+defineProps({
+  nodes: {
+    type: Array,
+    default: () => [],
+  },
+  edges: {
+    type: Array,
+    default: () => [],
+  },
+})
+</script>
+
+<template>
+  <VueFlow :nodes="nodes" :edges="edges">
+    <MiniMap />
+  </VueFlow>
+</template>
