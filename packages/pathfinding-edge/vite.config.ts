8df92fb311c21b54a0ec1c447b6e28632a5266ae
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-08 21:53:48 +0200

    chore(deps-dev): update deps and bump versions

--- packages/pathfinding-edge/vite.config.ts
+++ packages/pathfinding-edge/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
     extensions: ['.ts', '.vue'],
   },
   build: {
-    minify: 'esbuild',
     emptyOutDir: false,
     lib: {
       formats: ['es', 'cjs', 'iife'],
@@ -24,13 +23,14 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ['vue', '@vue-flow/core'],
       output: {
         dir: './dist',
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          'vue': 'Vue',
+          '@vue-flow/core': 'VueFlow',
         },
       },
     },
