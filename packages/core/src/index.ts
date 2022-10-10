c4dba9fceae61ea45f682be2f51a63897b93b289
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-10 15:42:20 +0200

    refactor(core): rename core pkg dir to `core`

---
+++ packages/core/src/index.ts
@@ -0,0 +1,50 @@
+/**
+ * Vue Flow
+ * @module vue-flow
+ */
+
+export { default as VueFlow } from './container/VueFlow/VueFlow.vue'
+
+export { default as Handle } from './components/Handle/Handle.vue'
+
+export { StraightEdge, StepEdge, BezierEdge, SimpleBezierEdge, SmoothStepEdge, BaseEdge, EdgeText } from './components/Edges'
+
+export { getBezierPath, getSimpleBezierPath, getSmoothStepPath, getStraightPath } from './components/Edges/utils'
+
+export {
+  isNode,
+  isEdge,
+  addEdge,
+  updateEdge,
+  getOutgoers,
+  getIncomers,
+  getConnectedEdges,
+  getTransformForBounds,
+  getRectOfNodes,
+  graphPosToZoomedPos,
+  getNodesInside,
+  getMarkerId,
+  getBoundsofRects,
+} from './utils/graph'
+
+/**
+ * Intended for options API
+ * In composition API you can access apply utilities from `useVueFlow`
+ */
+export { applyChanges, applyEdgeChanges, applyNodeChanges } from './utils/changes'
+
+export { defaultEdgeTypes, defaultNodeTypes } from './store'
+
+export { VueFlow as VueFlowInjection, NodeId as NodeIdInjection } from './context'
+
+export { default as useZoomPanHelper } from './composables/useZoomPanHelper'
+
+export { default as useVueFlow, Storage as GlobalVueFlowStorage } from './composables/useVueFlow'
+
+export { default as useHandle } from './composables/useHandle'
+
+export { default as useNode } from './composables/useNode'
+
+export { default as useEdge } from './composables/useEdge'
+
+export * from './types'
