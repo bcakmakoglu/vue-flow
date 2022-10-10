082050b1648dd3c6569cab0cd974cf9128647c98
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-10 15:42:20 +0200

    refactor(core): rename core pkg dir to `core`

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'no-use-before-define': 0,
+    'vue/no-setup-props-destructure': 0,
+    'react/prop-types': 0,
+  },
+  extends: ['../../.eslintrc.js'],
+  ignorePatterns: ['!**/*'],
+}
