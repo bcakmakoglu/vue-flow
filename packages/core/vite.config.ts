e174abc77e80bcb7b7c83f91b24341e6d519ad6b
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-10 15:42:20 +0200

    refactor(core): rename core pkg dir to `core`

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,57 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueTypes from 'vite-plugin-vue-type-imports'
+import AutoImport from 'unplugin-auto-import/vite'
+import replace from '@rollup/plugin-replace'
+import pkg from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+    },
+    extensions: ['.ts', '.vue'],
+  },
+  build: {
+    emptyOutDir: false,
+    lib: {
+      formats: ['es', 'cjs', 'iife'],
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'vue-flow-core',
+      name: 'vueFlowCore',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        format: 'esm',
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    vueTypes(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core', 'vue/macros'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    replace({
+      __VUE_FLOW_VERSION__: JSON.stringify(pkg.version),
+      preventAssignment: true,
+    }),
+  ],
+  optimizeDeps: {
+    include: ['@vueuse/core', 'd3-zoom', 'd3-selection', 'd3-drag'],
+  },
+})
