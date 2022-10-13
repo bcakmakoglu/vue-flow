20d026977083d9134e3f4d9e1a6ea6650e711f6c
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-13 16:26:44 +0200

    chore(core): plugin hook name

--- packages/core/src/types/plugins.ts
+++ packages/core/src/types/plugins.ts
@@ -3,7 +3,7 @@ import type { FlowOptions } from './flow'
 import type { VueFlowStore } from './store'
 
 export interface PluginHooks {
-  onBeforeCreate: EventHookOn<[string, FlowOptions | undefined]>
+  beforeCreate: EventHookOn<[string, FlowOptions | undefined]>
   created: EventHookOn<VueFlowStore>
   beforeDestroy: EventHookOn<VueFlowStore>
   destroyed: EventHookOn<string>
