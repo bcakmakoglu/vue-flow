27ef3a6c6ffd86a4ee2508f7512be0c4fe9d49ca
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-18 21:37:42 +0200

    examples: only install one plugin per `use` call

--- examples/vite/main.ts
+++ examples/vite/main.ts
@@ -1,18 +1,21 @@
 import { createApp } from 'vue'
 import { createVueFlow } from '@vue-flow/core'
-import { PluginDagreLayout } from '@vue-flow/plugin-dagre'
-import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import { PluginScreenshot } from '@vue-flow/plugin-screenshot'
 import './index.css'
+import { createVueFlow } from '@vue-flow/core'
+import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import App from './App.vue'
 import { router } from './router'
 
-const app = createApp(App)
 const vueFlowApp = createVueFlow()
 
-vueFlowApp.use(PluginDagreLayout)
-vueFlowApp.use(PluginDragNDrop)
 vueFlowApp.use(PluginScreenshot)
+
+const app = createApp(App)
+
+const vueFlowApp = createVueFlow()
+
+vueFlowApp.use(PluginDragNDrop)
 
 app.config.performance = true
 app.use(router)
