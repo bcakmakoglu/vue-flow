f94cc3c18b4dc9006c1395c1f3838ca1c57b2eae
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-19 00:03:03 +0200

    feat(plugins): add screenshot plugin

---
+++ packages/plugins/screenshot/vite.config.ts
@@ -0,0 +1,35 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    extensions: ['.ts'],
+  },
+  build: {
+    emptyOutDir: false,
+    lib: {
+      formats: ['es', 'cjs', 'iife'],
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'vue-flow-plugin-screenshot',
+      name: 'vueFlowPluginScreenshot',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', '@vue-flow/core'],
+      output: {
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'vue': 'Vue',
+          '@vue-flow/core': 'VueFlow',
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', '@vueuse/core'],
+  },
+})
