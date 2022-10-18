f20e0055129b465f1351372b162e3bc7400f743f
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-18 21:19:13 +0200

    examples: add drag and drop plugin to dnd example

--- examples/vite/src/DragNDrop/DnD.vue
+++ examples/vite/src/DragNDrop/DnD.vue
@@ -1,11 +1,10 @@
 <script lang="ts" setup>
 import { VueFlow, useVueFlow } from '@vue-flow/core'
+import { useDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import Sidebar from './Sidebar.vue'
 
 let id = 0
-function getId() {
-  return `dndnode_${id++}`
-}
+const getId = () => `dndnode_${id++}`
 
 const { onConnect, addEdges, addNodes, project } = useVueFlow({
   nodes: [
@@ -17,39 +16,28 @@ const { onConnect, addEdges, addNodes, project } = useVueFlow({
     },
   ],
 })
-function onDragOver(event: DragEvent) {
-  event.preventDefault()
-  if (event.dataTransfer) {
-    event.dataTransfer.dropEffect = 'move'
-  }
-}
 
-const wrapper = ref()
+const { handleDragOver, handleDrop, onDrop } = useDragNDrop()
 
-onConnect(addEdges)
+onConnect((params) => addEdges([params]))
 
-function onDrop(event: DragEvent) {
-  const type = event.dataTransfer?.getData('application/vueflow')
+onDrop(({ type, position }) => {
+  addNodes([
+    {
+      id: getId(),
+      type,
+      position: project(position),
+      label: `${type} node`,
+    },
 
-  const flowbounds = wrapper.value.$el.getBoundingClientRect()
-
-  const position = project({
-    x: event.clientX - flowbounds.left,
-    y: event.clientY - flowbounds.top,
-  })
-
-  addNodes({
-    id: getId(),
-    type,
-    position,
-    label: `${type} node`,
-  })
-}
+  ])
+})
 </script>
 
 <template>
-  <div class="dndflow" @drop="onDrop">
-    <VueFlow ref="wrapper" @dragover="onDragOver" />
+  <div class="dndflow" @drop="handleDrop">
+    <VueFlow @dragover="handleDragOver" />
+
     <Sidebar />
   </div>
 </template>
