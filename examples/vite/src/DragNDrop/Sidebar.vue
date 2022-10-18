b76ccbe95c2fd6e1f1fe89c492f30a799f06af1c
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-10-18 21:19:13 +0200

    examples: add drag and drop plugin to dnd example

--- examples/vite/src/DragNDrop/Sidebar.vue
+++ examples/vite/src/DragNDrop/Sidebar.vue
@@ -1,23 +1,14 @@
 <script lang="ts" setup>
-const onDragStart = (event: DragEvent, nodeType: string) => {
-  if (event.dataTransfer) {
-    event.dataTransfer.setData('application/vueflow', nodeType)
-    event.dataTransfer.effectAllowed = 'move'
-  }
-}
+import { useDragNDrop } from '@vue-flow/plugin-drag-n-drop'
+
+const { handleDragStart } = useDragNDrop()
 </script>
 
 <template>
   <aside>
     <div class="description">You can drag these nodes to the pane on the left.</div>
-    <div class="vue-flow__node-input" :draggable="true" @dragstart="(event: DragEvent) => onDragStart(event, 'input')">
-      Input Node
-    </div>
-    <div class="vue-flow__node-default" :draggable="true" @dragstart="(event: DragEvent) => onDragStart(event, 'default')">
-      Default Node
-    </div>
-    <div class="vue-flow__node-output" :draggable="true" @dragstart="(event: DragEvent) => onDragStart(event, 'output')">
-      Output Node
-    </div>
+    <div class="vue-flow__node-input" :draggable="true" @dragstart="handleDragStart($event, 'input')">Input Node</div>
+    <div class="vue-flow__node-default" :draggable="true" @dragstart="handleDragStart($event, 'default')">Default Node</div>
+    <div class="vue-flow__node-output" :draggable="true" @dragstart="handleDragStart($event, 'output')">Output Node</div>
   </aside>
 </template>
