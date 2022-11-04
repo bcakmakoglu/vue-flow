f0e534c3e78a5a9aa7b9fd9d34591017194e6ee8
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-05 00:31:11 +0100

    chore: update pnpm-lock.yaml

--- examples/vite/main.ts
+++ examples/vite/main.ts
@@ -1,16 +1,16 @@
 import { createApp } from 'vue'
+import { createVueFlow } from '@vue-flow/core'
+import { PluginDagreLayout } from '@vue-flow/plugin-dagre'
+import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import { PluginScreenshot } from '@vue-flow/plugin-screenshot'
 import './index.css'
-import { createVueFlow } from '@vue-flow/core'
-import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
-import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 
 const app = createApp(App)
-
 const vueFlowApp = createVueFlow()
 
+vueFlowApp.use(PluginDagreLayout)
 vueFlowApp.use(PluginDragNDrop)
 vueFlowApp.use(PluginScreenshot)
 
