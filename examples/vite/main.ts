ba9f429ff977bd71497c2bbad7841ad93432b159
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-05 00:31:11 +0100

    chore: update pnpm-lock.yaml

--- examples/vite/main.ts
+++ examples/vite/main.ts
@@ -1,21 +1,18 @@
 import { createApp } from 'vue'
 import { createVueFlow } from '@vue-flow/core'
+import { PluginDagreLayout } from '@vue-flow/plugin-dagre'
+import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import { PluginScreenshot } from '@vue-flow/plugin-screenshot'
 import './index.css'
-import { createVueFlow } from '@vue-flow/core'
-import { PluginDragNDrop } from '@vue-flow/plugin-drag-n-drop'
 import App from './App.vue'
 import { router } from './router'
 
-const vueFlowApp = createVueFlow()
-
-vueFlowApp.use(PluginScreenshot)
-
 const app = createApp(App)
-
 const vueFlowApp = createVueFlow()
 
+vueFlowApp.use(PluginDagreLayout)
 vueFlowApp.use(PluginDragNDrop)
+vueFlowApp.use(PluginScreenshot)
 
 app.config.performance = true
 app.use(router)
