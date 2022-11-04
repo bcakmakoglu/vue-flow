daae09f2369f54fcf43c04c683062d30c1dc439f
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-05 00:14:28 +0100

    examples: use dagre plugin in layouting example

--- examples/vite/main.ts
+++ examples/vite/main.ts
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
 import { createVueFlow } from '@vue-flow/core'
+import { PluginDagreLayout } from '@vue-flow/plugin-dagre'
 import { PluginScreenshot } from '@vue-flow/plugin-screenshot'
 import './index.css'
 import App from './App.vue'
 import { router } from './router'
 
+const app = createApp(App)
 const vueFlowApp = createVueFlow()
 
+vueFlowApp.use(PluginDagreLayout)
 vueFlowApp.use(PluginScreenshot)
-
-const app = createApp(App)
 
 app.config.performance = true
 app.use(router)
