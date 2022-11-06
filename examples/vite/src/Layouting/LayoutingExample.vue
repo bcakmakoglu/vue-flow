4b37cb294dfca6d89952ba4f93ce84a54ecd2144
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-06 19:40:49 +0100

    chore: update pnpm-lock.yaml

--- examples/vite/src/Layouting/LayoutingExample.vue
+++ examples/vite/src/Layouting/LayoutingExample.vue
@@ -7,13 +7,11 @@ import initialElements from './initial-elements'
 const elements = ref(initialElements)
 
 const { layout } = useDagreLayout()
-
-const onPaneReady = () => setTimeout(() => layout('TB'))
 </script>
 
 <template>
   <div style="flex: 1; position: relative">
-    <VueFlow v-model="elements" @pane-ready="onPaneReady">
+    <VueFlow v-model="elements" @nodes-initialized="layout('TB')">
       <Panel :position="PanelPosition.TopRight">
         <button :style="{ marginRight: '10px' }" @click="layout('TB')">vertical layout</button>
         <button @click="layout('LR')">horizontal layout</button>
