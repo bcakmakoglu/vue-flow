283a5410eb6406e8cd31e5ef51fd18b7bdc20463
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-06 19:48:25 +0100

    chore(examples): update layouting example

--- examples/vite/src/Layouting/LayoutingExample.vue
+++ examples/vite/src/Layouting/LayoutingExample.vue
@@ -1,20 +1,30 @@
 <script lang="ts" setup>
-import { Panel, PanelPosition } from '@vue-flow/additional-components'
-import { VueFlow, Panel, PanelPosition } from '@vue-flow/core'
+import { VueFlow, useVueFlow, Panel, PanelPosition } from '@vue-flow/core'
+import type { Direction } from '@vue-flow/plugin-dagre'
 import { useDagreLayout } from '@vue-flow/plugin-dagre'
 import initialElements from './initial-elements'
 
 const elements = ref(initialElements)
 
+const { fitView } = useVueFlow()
+
 const { layout } = useDagreLayout()
+
+const onLayout = (direction: Direction) => {
+  layout(direction)
+
+  nextTick(() => {
+    fitView()
+  })
+}
 </script>
 
 <template>
   <div style="flex: 1; position: relative">
-    <VueFlow v-model="elements" @nodes-initialized="layout('TB')">
+    <VueFlow v-model="elements" @nodes-initialized="onLayout('TB')">
       <Panel :position="PanelPosition.TopRight">
-        <button :style="{ marginRight: '10px' }" @click="layout('TB')">vertical layout</button>
-        <button @click="layout('LR')">horizontal layout</button>
+        <button :style="{ marginRight: '10px' }" @click="onLayout('TB')">vertical layout</button>
+        <button @click="onLayout('LR')">horizontal layout</button>
       </Panel>
     </VueFlow>
   </div>
