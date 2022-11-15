bce493cfe5cb954c55c2f16b7f025634b361546a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-11-15 12:51:40 +0100

    docs: add vercel analytics

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import VueAnimXyz from '@animxyz/vue3'
 import DefaultTheme from 'vitepress/theme'
+import { inject } from '@vercel/analytics'
 import Layout from './layouts/default.vue'
 import 'virtual:windi.css'
 import '@animxyz/core'
@@ -13,6 +14,7 @@ const CustomTheme = {
   Layout,
   enhanceApp: ({ app }) => {
     app.use(VueAnimXyz)
+    inject()
   },
 } as Theme
 
