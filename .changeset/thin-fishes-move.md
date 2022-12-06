c673b044227a6702c695691be6f94f4331812dec
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-06 15:57:34 +0100

    fix(edges): type marker prop in `EdgeProps`
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

---
+++ .changeset/thin-fishes-move.md
@@ -0,0 +1,5 @@
+---
+'@vue-flow/core': patch
+---
+
+Type `markerEnd` and `markerStart` in `EdgeProps`
--- packages/pathfinding-edge/src/types.ts
+++ packages/pathfinding-edge/src/types.ts
@@ -21,8 +21,8 @@ export interface PathFindingEdgeProps extends EdgeProps<never> {
   labelBgPadding?: [number, number]
   labelBgBorderRadius?: number
   style?: CSSProperties
-  markerEnd?: string
-  markerStart?: string
+  markerEnd: string
+  markerStart: string
   sourceHandleId?: string
   targetHandleId?: string
 }
@@ -46,8 +46,8 @@ export interface PerfectArrowProps {
   labelBgPadding?: [number, number]
   labelBgBorderRadius?: number
   style?: CSSProperties
-  markerEnd?: string
-  markerStart?: string
+  markerEnd: string
+  markerStart: string
   sourceHandleId?: string
   targetHandleId?: string
   options?: ArrowOptions
