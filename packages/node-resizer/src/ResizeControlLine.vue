d883ed782a13f9feb22f78ee5f8423f3b8e6d8ad
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-09 20:19:59 +0100

    feat(node-resizer): implement node resizer components
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

---
+++ packages/node-resizer/src/ResizeControlLine.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import ResizeControl from './ResizeControl.vue'
+import type { ResizeControlLineProps, ResizeDragEvent, ResizeEventParams } from './types'
+import { ResizeControlVariant } from './types'
+
+const props = defineProps<ResizeControlLineProps>()
+
+const emits = defineEmits<{
+  (event: 'resizeStart', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+  (event: 'resize', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+  (event: 'resizeEnd', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+}>()
+</script>
+
+<template>
+  <ResizeControl
+    v-bind="props"
+    :variant="ResizeControlVariant.Line"
+    @resize-start="emits('resizeStart', $event)"
+    @resize="emits('resize', $event)"
+    @resize-end="emits('resizeEnd', $event)"
+  />
+</template>
--- packages/node-resizer/src/types.ts
+++ packages/node-resizer/src/types.ts
@@ -1,8 +1,56 @@
-import type { Position } from '@vue-flow/core'
+import type { D3DragEvent, SubjectPosition } from 'd3-drag'
+import type { CSSProperties } from 'vue'
 
-export interface NodeToolbarProps {
-  nodeId?: string | string[]
-  isVisible?: boolean
-  position?: Position
-  offset?: number
+export interface ResizeEventParams {
+  x: number
+  y: number
+  width: number
+  height: number
 }
+
+export interface NodeResizerProps {
+  nodeId?: string
+  color?: string
+  handleClassName?: string
+  handleStyle?: CSSProperties
+  lineClassName?: string
+  lineStyle?: CSSProperties
+  isVisible?: boolean
+  minWidth?: number
+  minHeight?: number
+}
+
+export interface NodeResizerEmits {
+  (event: 'resizeStart', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+  (event: 'resize', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+  (event: 'resizeEnd', data: { event: ResizeDragEvent; params: ResizeEventParams }): void
+}
+
+export type ControlLinePosition = 'top' | 'bottom' | 'left' | 'right'
+
+export type ControlPosition = ControlLinePosition | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+
+export enum ResizeControlVariant {
+  Line = 'line',
+  Handle = 'handle',
+}
+
+export interface ResizeControlProps {
+  nodeId?: string
+  color?: string
+  minWidth?: number
+  minHeight?: number
+  position?: ControlPosition
+  variant?: ResizeControlVariant
+}
+
+export interface ResizeControlLineProps {
+  nodeId?: string
+  color?: string
+  minWidth?: number
+  minHeight?: number
+  variant?: ResizeControlVariant
+  position?: ControlLinePosition
+}
+
+export type ResizeDragEvent = D3DragEvent<HTMLDivElement, null, SubjectPosition>
