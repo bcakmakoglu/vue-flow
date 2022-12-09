31547968aa39ef0f3daf18533a2a5db1e4f1ec78
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-09 20:19:12 +0100

    chore(examples): cleanup
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- examples/vite/src/Basic/Basic.vue
+++ examples/vite/src/Basic/Basic.vue
@@ -11,7 +11,7 @@ const elements = ref<Elements>([
   { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, class: 'light' },
   { id: '3', label: 'Node 3', position: { x: 400, y: 100 }, class: 'light' },
   { id: '4', label: 'Node 4', position: { x: 400, y: 200 }, class: 'light' },
-  { id: 'e1-2', source: '1', target: '2', animated: true, label: 'foobar' },
+  { id: 'e1-2', source: '1', target: '2', animated: true },
   { id: 'e1-3', source: '1', target: '3' },
 ])
 const { onPaneReady, onNodeDragStop, onConnect, addEdges, setTransform, toObject } = useVueFlow({
