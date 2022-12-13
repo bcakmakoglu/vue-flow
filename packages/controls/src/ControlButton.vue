a55f39ba250cee7adce3e1ca99b98ed60f278541
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-13 14:31:09 +0100

    refactor(controls): move controls into separate package
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

---
+++ packages/controls/src/ControlButton.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+  name: 'ControlButton',
+}
+</script>
+
+<template>
+  <button class="vue-flow__controls-button">
+    <slot></slot>
+  </button>
+</template>
---
+++ packages/controls/vite.config.ts
@@ -0,0 +1,45 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import vueTypes from 'vite-plugin-vue-type-imports'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    extensions: ['.ts', '.vue'],
+  },
+  build: {
+    emptyOutDir: false,
+    lib: {
+      formats: ['es', 'cjs', 'iife'],
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'vue-flow-controls',
+      name: 'vueFlowControls',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', '@vue-flow/core'],
+      output: {
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'vue': 'Vue',
+          '@vue-flow/core': 'VueFlow',
+        },
+      },
+    },
+  },
+  plugins: [
+    vue({
+      reactivityTransform: true,
+    }),
+    vueTypes(),
+    AutoImport({
+      imports: ['vue', 'vue/macros'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+  ],
+})
