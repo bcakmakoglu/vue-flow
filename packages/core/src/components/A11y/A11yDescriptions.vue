f64e29850879b5a2ee2e4e332ab5ef9726fe7297
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-18 21:09:26 +0100

    feat(core): add `A11yDescriptions` component
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

---
+++ packages/core/src/components/A11y/A11yDescriptions.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const { id, disableKeyboardA11y } = useVueFlow()
+</script>
+
+<template>
+  <template v-if="!disableKeyboardA11y">
+    <div :id="`${ARIA_NODE_DESC_KEY}-${id}`" style="display: none">
+      Press enter or space to select a node. You can then use the arrow keys to move the node around, press delete to remove it
+      and press escape to cancel.
+    </div>
+
+    <div :id="`${ARIA_EDGE_DESC_KEY}-${id}`" style="display: none">
+      Press enter or space to select an edge. You can then press delete to remove it or press escape to cancel.
+    </div>
+  </template>
+</template>
---
+++ packages/core/src/utils/a11y.ts
@@ -0,0 +1,4 @@
+export const ARIA_NODE_DESC_KEY = 'vue-flow__node-desc'
+export const ARIA_EDGE_DESC_KEY = 'vue-flow__edge-desc'
+
+export const elementSelectionKeys = ['Enter', ' ', 'Escape']
