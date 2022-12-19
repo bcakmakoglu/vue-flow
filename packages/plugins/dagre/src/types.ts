3ef2b9ea5dc687ab4c9dca6707edd636a679d6b7
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-19 21:38:24 +0100

    feat(plugin-dagre): add `direction` to dagre state
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/plugins/dagre/src/types.ts
+++ packages/plugins/dagre/src/types.ts
@@ -1,8 +1,10 @@
 import type dagre from 'dagre'
+import type { Ref } from 'vue'
 
 export type Direction = 'TB' | 'BT' | 'LR' | 'RL'
 
 export interface UseDagreState {
-  layout: (direction: Direction) => void
+  layout: (direction?: Direction) => void
   dagreGraph: dagre.graphlib.Graph
+  direction: Ref<Direction | undefined>
 }
