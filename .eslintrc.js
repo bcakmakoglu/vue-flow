3dfab8f5d9fe795f48071deff64278020da99d53
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-25 11:33:19 +0100

    chore: add eslint rule to show error on unused vars
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const baseRules = {
   'vue/no-setup-props-destructure': 0,
   'no-console': 0,
   'chai-friendly/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+  '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
   'prettier/prettier': ['error', {}, { usePrettierrc: true }],
   'antfu/if-newline': 0,
   'antfu/generic-spacing': 0,
