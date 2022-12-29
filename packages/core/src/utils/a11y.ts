47a84bfd34f041cb045205b10aa4687ac9e26082
braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-12-29 14:03:06 +0100

    fix(a11y): use correct vue flow aria names
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/utils/a11y.ts
+++ packages/core/src/utils/a11y.ts
@@ -3,7 +3,7 @@ import type { XYPosition } from '~/types'
 export const ARIA_NODE_DESC_KEY = 'vue-flow__node-desc'
 export const ARIA_EDGE_DESC_KEY = 'vue-flow__edge-desc'
 
-export const ARIA_LIVE_MESSAGE = 'react-flow__aria-live'
+export const ARIA_LIVE_MESSAGE = 'vue-flow__aria-live'
 
 export const elementSelectionKeys = ['Enter', ' ', 'Escape']
 
