941f37c821e4adb9ea28ec133eda05d2ab45ee2b
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-02-01 23:50:39 +0100

    feat(core): make `useNode` and `useEdge` reactive
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/composables/useEdge.ts
+++ packages/core/src/composables/useEdge.ts
@@ -1,4 +1,5 @@
-import { inject } from 'vue'
+import type { MaybeRef } from '@vueuse/core'
+import { computed, inject } from 'vue'
 import { useVueFlow } from './useVueFlow'
 import type { GraphEdge } from '~/types'
 import { ErrorCode, VueFlowError } from '~/utils'
@@ -11,15 +12,25 @@ import { EdgeId, EdgeRef } from '~/context'
  *
  * Meaning if you do not provide an id, this composable has to be called in a child of your custom edge component, or it will throw
  */
-export function useEdge<T extends GraphEdge = GraphEdge>(id?: string) {
-  const edgeId = id ?? inject(EdgeId, '')
-  const edgeEl = inject(EdgeRef, null)
-
+export function useEdge<T extends GraphEdge = GraphEdge>(
+  id?: MaybeRef<string>,
+) {
   const { findEdge, emits } = useVueFlow()
 
-  const edge = findEdge<T>(edgeId)
+  const edgeRef = inject(EdgeRef, null)
 
-  if (!edge) {
+  const edgeIdInjection = inject(EdgeId, '')
+
+  const edgeId = computed(() => unref(id) ?? edgeIdInjection)
+
+  const edgeEl = computed(() => unref(edgeRef) ?? document.querySelector(`[data-id="${edgeId.value}"]`))
+
+  const edge = computed(() => findEdge<T>(edgeId.value)!)
+
+  // todo: use watcher to throw or throw in computed
+  if (!edgeId.value || edgeId.value === '') {
+    throw new VueFlowError(`No edge id provided and no injection could be found!`, 'useEdge')
+  } else if (!edge.value) {
     emits.error(new VueFlowError(ErrorCode.EDGE_NOT_FOUND, edgeId))
   }
 
--- packages/core/src/composables/useNode.ts
+++ packages/core/src/composables/useNode.ts
@@ -1,5 +1,6 @@
 import { computed, inject } from 'vue'
 import { useVueFlow } from './useVueFlow'
+import type { MaybeRef } from '@vueuse/core'
 import type { GraphNode } from '~/types'
 import { NodeId, NodeRef } from '~/context'
 import { ErrorCode, VueFlowError, getConnectedEdges } from '~/utils'
@@ -11,15 +12,25 @@ import { ErrorCode, VueFlowError, getConnectedEdges } from '~/utils'
  *
  * Meaning if you do not provide an id, this composable has to be called in a child of your custom node component, or it will throw
  */
-export function useNode<T extends GraphNode = GraphNode>(id?: string) {
-  const nodeId = id ?? inject(NodeId, '')
-  const nodeEl = inject(NodeRef, null)
-
+export function useNode<T extends GraphNode = GraphNode>(
+  id?: MaybeRef<string>,
+) {
   const { findNode, edges, emits } = useVueFlow()
 
-  const node = findNode<T>(nodeId)!
+  const nodeIdInjection = inject(NodeId, '')
 
-  if (!node) {
+  const nodeId = computed(() => unref(id) ?? nodeIdInjection)
+
+  const nodeRef = inject(NodeRef, null)
+
+  const nodeEl = computed(() => unref(nodeRef) ?? document.querySelector(`[data-id="${nodeId.value}"]`))!
+
+  const node = computed(() => findNode<T>(nodeId.value)!)
+
+  // todo: use watcher or computed to throw
+  if (!nodeId.value || nodeId.value === '') {
+    throw new VueFlowError(`useNode - No node id provided and no injection could be found!`, 'useNode')
+  } else if (!node.value) {
     emits.error(new VueFlowError(ErrorCode.NODE_NOT_FOUND, nodeId))
   }
 
