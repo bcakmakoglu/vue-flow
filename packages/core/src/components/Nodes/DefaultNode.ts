0ca4bad2dbcb071d3907a7e8451cf6060caa8039
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-02-17 12:41:32 +0100

    chore(core): update functional component types
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/components/Nodes/DefaultNode.ts
+++ packages/core/src/components/Nodes/DefaultNode.ts
@@ -1,4 +1,4 @@
-import type { FunctionalComponent } from 'vue'
+import type { Component, FunctionalComponent } from 'vue'
 import Handle from '../Handle/Handle.vue'
 import type { NodeProps } from '~/types'
 import { Position } from '~/types'
@@ -12,9 +12,9 @@ const DefaultNode: FunctionalComponent<NodeProps> = function ({
   isValidSourcePos,
 }) {
   return [
-    h(Handle, { type: 'target', position: targetPosition, connectable, isValidConnection: isValidTargetPos }),
+    h(Handle as Component, { type: 'target', position: targetPosition, connectable, isValidConnection: isValidTargetPos }),
     typeof label !== 'string' && label ? h(label) : h('div', { innerHTML: label }),
-    h(Handle, { type: 'source', position: sourcePosition, connectable, isValidConnection: isValidSourcePos }),
+    h(Handle as Component, { type: 'source', position: sourcePosition, connectable, isValidConnection: isValidSourcePos }),
   ]
 }
 
