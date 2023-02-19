a51c6309629a9280d9bb250f9488be8ef01a9920
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-02-19 17:41:05 +0100

    chore(docus): update deps
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- docs2/app.config.ts
+++ docs2/app.config.ts
@@ -1,28 +1,39 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
+    title: 'Vue Flow',
+    description: 'Visualize your ideas with Vue Flow, a highly customizable Vue3 Flowchart library.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_themes',
-      github: 'nuxt-themes/docus',
+      twitter: 'bcakmakoglu',
+      github: 'bcakmakoglu/vue-flow',
+    },
+    github: {
+      branch: 'master',
+      repo: 'bcakmakoglu/vue-flow',
+      owner: 'bcakmakoglu',
+      edit: true,
     },
     aside: {
       level: 0,
-      exclude: []
+      exclude: [],
     },
     header: {
       logo: true,
       showLinkIcon: true,
-      exclude: []
+      exclude: [],
     },
     footer: {
+      credits: {
+        text: 'Copyright © 2021-present Burak Cakmakoglu',
+        href: 'https://github.com/bcakmakoglu/vue-flow/blob/master/LICENSE',
+        icon: '',
+      },
       iconLinks: [
         {
-          href: 'https://nuxt.com',
-          icon: 'IconNuxtLabs'
-        }
-      ]
-    }
-  }
+          href: 'https://discord.gg/F4v6qE4Fuq',
+          icon: 'ic:baseline-discord',
+        },
+      ],
+    },
+  },
 })
