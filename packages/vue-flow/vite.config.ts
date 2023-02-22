b33122383850c53ffe6d130c7f0d93992079f84e
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-02-22 09:47:34 +0100

    feat(tooling): use vite-config tooling pkg
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/vue-flow/vite.config.ts
+++ packages/vue-flow/vite.config.ts
@@ -1,35 +1,13 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { withConfig } from '@vue-flow/vite-config'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '~': resolve('src'),
-    },
-    extensions: ['.ts'],
-  },
+export default withConfig({
   build: {
-    emptyOutDir: false,
     lib: {
       formats: ['es', 'cjs', 'iife'],
       entry: resolve(__dirname, 'src/index.ts'),
       fileName: 'vue-flow',
       name: 'vueFlow',
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        format: 'esm',
-        dir: './dist',
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue',
-        },
-      },
     },
   },
 })
