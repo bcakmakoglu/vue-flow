36ba119f46d21836820edadcbbcaa6a17e14decc
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-02-25 00:03:18 +0100

    refactor(core): cleanup node styles
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/container/Viewport/Transform.vue
+++ packages/core/src/container/Viewport/Transform.vue
@@ -4,8 +4,6 @@ import EdgeRenderer from '../EdgeRenderer/EdgeRenderer.vue'
 
 const { id, viewport, emits, d3Zoom, d3Selection, dimensions, ...rest } = useVueFlow()
 
-let isReady = $ref(false)
-
 until(() => d3Zoom.value && d3Selection.value && dimensions.value.width > 0 && dimensions.value.height > 0)
   .toBeTruthy()
   .then(() => {
@@ -20,9 +18,6 @@ until(() => d3Zoom.value && d3Selection.value && dimensions.value.width > 0 && d
         dimensions,
         ...rest,
       })
-
-      // hide graph until nodes are ready, so we don't have jumping nodes
-      isReady = true
     }, 1)
   })
 </script>
@@ -38,7 +33,7 @@ export default {
   <div
     :key="`transform-${id}`"
     class="vue-flow__transformationpane vue-flow__container"
-    :style="{ transform: `translate(${viewport.x}px,${viewport.y}px) scale(${viewport.zoom})`, opacity: isReady ? undefined : 0 }"
+    :style="{ transform: `translate(${viewport.x}px,${viewport.y}px) scale(${viewport.zoom})` }"
   >
     <EdgeRenderer />
 
