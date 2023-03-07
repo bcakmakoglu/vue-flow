53d8d8399202f3a4d96d4136b037f189ea2c95b8
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-03-07 20:43:31 +0100

    feat(node-resizer): add maxHeight and maxWidth props
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/node-resizer/src/types.ts
+++ packages/node-resizer/src/types.ts
@@ -41,6 +41,8 @@ export interface NodeResizerProps {
   isVisible?: boolean
   minWidth?: number
   minHeight?: number
+  maxWidth?: number
+  maxHeight?: number
   shouldResize?: ShouldResize
   keepAspectRatio?: boolean | number
 }
@@ -65,17 +67,21 @@ export interface ResizeControlProps {
   color?: string
   minWidth?: number
   minHeight?: number
+  maxWidth?: number
+  maxHeight?: number
   position?: ControlPosition
   variant?: ResizeControlVariant
   shouldResize?: ShouldResize
   keepAspectRatio?: boolean | number
 }
 
-export interface ResizeControlLineProps {
+export interface ResizeControlLineProps extends ResizeControlProps {
   nodeId?: string
   color?: string
   minWidth?: number
   minHeight?: number
+  maxWidth?: number
+  maxHeight?: number
   variant?: ResizeControlVariant
   position?: ControlLinePosition
   keepAspectRatio?: boolean | number
