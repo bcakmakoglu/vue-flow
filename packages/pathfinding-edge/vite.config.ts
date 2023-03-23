cb0dc0a5071409c1ca5afa775d2a2d4a24742bbe
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-03-23 10:57:23 +0100

    chore(pathfinding-edge): update config
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/pathfinding-edge/vite.config.ts
+++ packages/pathfinding-edge/vite.config.ts
@@ -2,6 +2,11 @@ import { resolve } from 'node:path'
 import { withConfig } from '@tooling/vite-config'
 
 export default withConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './src'),
+    },
+  },
   build: {
     lib: {
       formats: ['es', 'cjs', 'iife'],
