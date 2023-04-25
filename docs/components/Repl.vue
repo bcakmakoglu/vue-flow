aa62e19ed9723d76798ba371cfb3397279c404d1
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-04-25 09:44:02 +0200

    chore(docs): render repl in ssr
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- docs/components/Repl.vue
+++ docs/components/Repl.vue
@@ -1,17 +1,12 @@
 <script lang="ts">
-import { inBrowser } from 'vitepress'
 import { Suspense } from 'vue'
 
 const DocsRepl = defineAsyncComponent(() => import('./DocsRepl.vue'))
 
 export default defineComponent({
-  props: ['example'],
+  props: ['example', 'examplesImports', 'dependencies'],
   setup(props) {
     return () => {
-      if (!inBrowser) {
-        return null
-      }
-
       return h(Suspense, h(DocsRepl, props))
     }
   },
