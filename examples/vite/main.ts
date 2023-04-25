18aeefb0694ec2dfc2a9bf11100a9023f0a57f8d
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-04-25 21:37:57 +0200

    feat(examples): add pinia example to vite-examples
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- examples/vite/main.ts
+++ examples/vite/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './index.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
 
@@ -7,4 +8,5 @@ const app = createApp(App)
 
 app.config.performance = true
 app.use(router)
+app.use(createPinia())
 app.mount('#root')
