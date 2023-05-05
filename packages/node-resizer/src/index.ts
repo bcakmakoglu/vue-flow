ed4eaac34e465595dc0a8b237809ccf1f064ab75
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-05-05 16:36:53 +0200

    refactor(node-resizer): add explicit imports and remove auto-imports
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/node-resizer/src/index.ts
+++ packages/node-resizer/src/index.ts
@@ -1,5 +1,6 @@
 import './style.css'
 
-export * from './types'
 export { default as NodeResizer } from './NodeResizer.vue'
 export { default as NodeResizeControl } from './ResizeControl.vue'
+
+export * from './types'
