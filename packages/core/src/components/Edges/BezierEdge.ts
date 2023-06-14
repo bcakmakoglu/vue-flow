7d93915f369d4665aced83b0e81102f08a88c5b7
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-06-14 21:53:08 +0200

    refactor(core): use normal components for edges

--- packages/core/src/components/Edges/BezierEdge.ts
+++ packages/core/src/components/Edges/BezierEdge.ts
@@ -1,49 +1,47 @@
-import type { FunctionalComponent } from 'vue'
-import { h } from 'vue'
+import { defineComponent, h } from 'vue'
 import BaseEdge from './BaseEdge.vue'
 import { getBezierPath } from './utils'
 import type { BezierEdgeProps } from '~/types'
 import { Position } from '~/types'
 
-const BezierEdge: FunctionalComponent<BezierEdgeProps> = function (
-  { sourcePosition = Position.Bottom, targetPosition = Position.Top, ...props },
-  { attrs },
-) {
-  const [path, labelX, labelY] = getBezierPath({
-    sourcePosition,
-    targetPosition,
-    ...props,
-  })
+const BezierEdge = defineComponent<BezierEdgeProps>({
+  name: 'BezierEdge',
+  props: [
+    'sourcePosition',
+    'targetPosition',
+    'label',
+    'labelStyle',
+    'labelShowBg',
+    'labelBgStyle',
+    'labelBgPadding',
+    'labelBgBorderRadius',
+    'sourceY',
+    'sourceX',
+    'targetX',
+    'targetY',
+    'curvature',
+    'markerEnd',
+    'markerStart',
+    'interactionWidth',
+  ] as any,
+  compatConfig: { MODE: 3 },
+  setup(props, { attrs }) {
+    return () => {
+      const [path, labelX, labelY] = getBezierPath({
+        ...props,
+        sourcePosition: props.sourcePosition ?? Position.Bottom,
+        targetPosition: props.targetPosition ?? Position.Top,
+      })
 
-  return h(BaseEdge as any, {
-    path,
-    labelX,
-    labelY,
-    ...props,
-    ...attrs,
-  })
-}
-
-BezierEdge.props = [
-  'sourcePosition',
-  'targetPosition',
-  'label',
-  'labelStyle',
-  'labelShowBg',
-  'labelBgStyle',
-  'labelBgPadding',
-  'labelBgBorderRadius',
-  'sourceY',
-  'sourceX',
-  'targetX',
-  'targetY',
-  'curvature',
-  'markerEnd',
-  'markerStart',
-  'interactionWidth',
-]
-
-BezierEdge.inheritAttrs = false
-BezierEdge.compatConfig = { MODE: 3 }
+      return h(BaseEdge as any, {
+        path,
+        labelX,
+        labelY,
+        ...attrs,
+        ...props,
+      })
+    }
+  },
+})
 
 export default BezierEdge
