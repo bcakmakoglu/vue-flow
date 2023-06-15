8713e98cbcda5d7489f426d21ba6b28bb1680454
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-06-15 22:09:41 +0200

    chore(core): cleanup

--- packages/core/src/composables/useHandle.ts
+++ packages/core/src/composables/useHandle.ts
@@ -46,7 +46,6 @@ export function useHandle({
     connectionRadius,
     connectOnClick,
     connectionClickStartHandle,
-    connectionEndHandle,
     nodesConnectable,
     autoPanOnConnect,
     findNode,
