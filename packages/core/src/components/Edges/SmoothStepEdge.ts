0596bc01e9f408820a4ff3e42b3f4aae0a31015d
Alex Tyshka <atyshka15@gmail.com>
2023-06-19 11:00:56 -0400

    Make loopback edges appear nicely

--- packages/core/src/components/Edges/SmoothStepEdge.ts
+++ packages/core/src/components/Edges/SmoothStepEdge.ts
@@ -2,17 +2,27 @@ import type { FunctionalComponent } from 'vue'
 import { h } from 'vue'
 import BaseEdge from './BaseEdge'
 import { getSmoothStepPath } from './utils'
-import type { SmoothStepEdgeProps } from '~/types'
+import type { SmoothStepEdgeProps, GraphNode } from '~/types'
 import { Position } from '~/types'
 
 const SmoothStepEdge: FunctionalComponent<SmoothStepEdgeProps> = function (
   { sourcePosition = Position.Bottom, targetPosition = Position.Top, ...props },
   { attrs },
 ) {
+  let centerX, centerY;
+  if (attrs.sourceNode == attrs.targetNode) {
+    if (sourcePosition == Position.Bottom && targetPosition == Position.Top) {
+      let source = attrs.sourceNode as GraphNode;
+      centerX = props.sourceX - (props.offset ?? 40) - source.dimensions.width / 2;
+      centerY = (props.sourceY + props.targetY) / 2;
+    }
+  }
   const [path, labelX, labelY] = getSmoothStepPath({
     sourcePosition,
     targetPosition,
-    ...props,
+    centerX,
+    centerY,
+    ...props, 
   })
 
   return h(BaseEdge, {
