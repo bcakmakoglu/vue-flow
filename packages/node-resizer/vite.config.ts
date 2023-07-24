9d4d15181ec266842c2768be6acd622dce171b0a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-07-25 01:07:13 +0200

    chore(node-resizer,deps): externalize d3 deps

--- packages/node-resizer/vite.config.ts
+++ packages/node-resizer/vite.config.ts
@@ -10,6 +10,22 @@ export default withConfig({
       fileName: 'vue-flow-node-resizer',
       name: 'VueFlowNodeResizer',
     },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', '@vue-flow/core', 'd3-drag', 'd3-selection'],
+      output: {
+        dir: './dist',
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          'vue': 'Vue',
+          '@vue-flow/core': 'VueFlowCore',
+          'd3-drag': 'd3Drag',
+          'd3-selection': 'd3Selection',
+        },
+      },
+    },
   },
   plugins: [vueTypes() as any],
 })
