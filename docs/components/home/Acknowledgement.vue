32a34f242270705b758c870ffa680f8f1c2e3da1
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-08-17 18:16:30 +0200

    docs: add showcase section to home page

--- docs/components/home/Acknowledgement.vue
+++ docs/components/home/Acknowledgement.vue
@@ -4,27 +4,34 @@ import Heart from '~icons/mdi/heart'
 
 <template>
   <div id="acknowledgement">
-    <div class="max-w-3/4 md:max-w-4xl mx-auto py-4 md:(pb-12 pt-6) text-center">
-      <h1 class="md:mb-8 flex justify-center items-center"><Heart class="text-red-500 mr-2" /> Acknowledgement</h1>
+    <div class="bg-black text-white dark:(bg-gray-100 text-black)">
+      <div class="max-w-3/4 md:max-w-4xl mx-auto py-4 md:py-12 lg:py-12 text-center">
+        <h1 class="md:(!mb-8) flex justify-center items-center gap-2"><Heart class="text-red-500" /> Acknowledgement</h1>
 
-      <p>
-        <strong>First off</strong>: A big thank you to the developers of
-        <a href="https://webkid.io" target="_blank" class="text-green-500">Webkid</a>. Their amazing work made it possible for me
-        to create this port to Vue 3 - without them there is no Vue Flow.
-      </p>
+        <p>
+          <span class="underline">First and foremost</span>: A huge <span class="font-semibold text-red-400">Thank you</span> to
+          the developers of <a href="https://webkid.io" target="_blank" class="text-green-500">Webkid</a>.
 
-      <br />
+          <span class="inline-block">
+            Without their continuous, amazing, open-source work, this project would not have been possible.
+          </span>
+        </p>
 
-      <p>
-        Vue Flow is built on top of existing features and code taken from
-        <a href="https://reactflow.dev" target="_blank" class="text-green-500">React Flow</a>. It replicates the basic features
-        found in React Flow (zoom, pan, graph, additional components and more) and brings them to the Vue 3 experience, with all
-        the fun reactivity and features like template slots etc. you know and love from Vue. If you're happy with Vue Flow,
-        <br />
-        <a href="https://github.com/sponsors/wbkd" target="_blank" class="text-lg font-bold text-green-500">
-          please consider supporting Webkid by donating.
-        </a>
-      </p>
+        <p>
+          <span class="font-semibold text-secondary">Vue Flow</span> is built on top of existing features and code taken from
+          <a href="https://reactflow.dev" target="_blank" class="text-green-500">React Flow</a>. It replicates the features found
+          in React Flow and brings them to the Vue 3 experience, with all the fun reactivity and features like template slots etc.
+          you know and love from Vue. If you're happy with Vue Flow,
+
+          <span class="inline-block text-xl font-medium">
+            please consider supporting <a href="https://webkid.io" target="_blank" class="text-green-500">Webkid</a> by
+            <a href="https://github.com/sponsors/wbkd" target="_blank" class="font-bold text-green-500"> donating </a> or
+            <a href="https://pro.reactflow.dev/" target="_blank" class="font-bold text-green-500">
+              subscribing to React Flow Pro.
+            </a>
+          </span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
