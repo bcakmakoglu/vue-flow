28fd4955f4a69e310e7f8f901efe85cf75a4336e
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-10-06 17:06:26 +0200

    docs: update theme

--- docs/components/home/Showcase.vue
+++ docs/components/home/Showcase.vue
@@ -2,13 +2,13 @@
 
 <template>
   <div class="w-full">
-    <div class="max-w-3/4 md:max-w-5xl mx-auto py-4 md:py-12 lg:py-12 text-center">
+    <div class="max-w-5/6 lg:max-w-7xl mx-auto py-4 md:py-12 lg:py-12 text-center">
       <div class="flex flex-col items-center justify-center gap-2">
         <h1>Showcase</h1>
 
         <p class="text-lg">Vue Flow is used in a variety of projects and applications. Here are some of them:</p>
 
-        <div class="flex flex-col md:flex-row justify-between gap-12 mt-8">
+        <div class="flex flex-col md:flex-row justify-between items-center gap-12 mt-8">
           <div class="flex-1 flex flex-col justify-center">
             <div
               class="h-[200px] md:min-h-[300px] w-full shadow-xl rounded-xl font-mono uppercase border-1 border-gray overflow-hidden"
@@ -20,7 +20,7 @@
               <h2 class="text-3xl mt-6 font-semibold text-[#2196f3]">Octai</h2>
             </a>
 
-            <h3 class="text-lg">Machine Learning NoCode Platform for Data Scientists and Developers.</h3>
+            <h3 class="text-lg">A Machine-Learning NoCode platform using a node-based UI for Data Scientists and Developers.</h3>
           </div>
 
           <div class="flex-1 flex flex-col justify-center">
--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -6,10 +6,10 @@ import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
 import '@vue-flow/controls/dist/style.css'
 import '@vue-flow/minimap/dist/style.css'
-import './../../assets/index.css'
 
 import Theme from 'vitepress/theme'
 import Layout from './layouts/default.vue'
+import './style.css'
 
 export default {
   extends: Theme,
