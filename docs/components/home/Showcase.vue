d0c61330b8f7712210f176c698a4b881c606f9d8
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-10-07 12:22:07 +0200

    docs: update home page styles

--- docs/components/home/Showcase.vue
+++ docs/components/home/Showcase.vue
@@ -1,17 +1,21 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="w-full text-white bg-gradient-to-br from-secondary-500 to-secondary-100 dark:(!bg-none)">
-    <div class="max-w-5/6 2xl:max-w-7xl mx-auto py-4 md:py-12 lg:py-12">
+  <div
+    class="border-t-1 border-secondary w-full text-white bg-gradient-to-br from-secondary-500 to-secondary-100 dark:(!bg-none)"
+  >
+    <div class="max-w-5/6 2xl:max-w-7xl mx-auto py-12 md:py-16">
       <div class="flex flex-col items-center justify-center gap-2">
         <h1>Putting Ideas into Action: The Showcase</h1>
 
-        <p class="font-medium max-w-3/4 xl:max-w-1/2">
+        <p class="font-medium max-w-3/4 text-center">
           Whether you're a seasoned developer or just starting your journey with Vue Flow, this showcase offers inspiration and
           insights into what's possible. Dive in and discover the potential of Vue Flow for your next project.
         </p>
 
-        <div class="flex flex-col md:flex-row justify-between items-center gap-12 mt-8 text-center md:text-left">
+        <div
+          class="divide-y divide-secondary md:divide-y-0 flex flex-col md:flex-row justify-between items-center gap-12 mt-8 text-center md:text-left"
+        >
           <div class="flex-1 flex flex-col items-center md:items-start">
             <div
               class="h-[200px] md:min-h-[300px] w-full shadow-xl rounded-xl font-mono uppercase border-1 border-gray overflow-hidden"
@@ -30,7 +34,7 @@
             </div>
           </div>
 
-          <div class="flex-1 flex flex-col justify-center">
+          <div class="pt-12 md:pt-0 flex-1 flex flex-col justify-center">
             <div
               class="h-[200px] md:min-h-[300px] w-full shadow-xl rounded-xl font-mono uppercase border-1 border-gray overflow-hidden"
             >
