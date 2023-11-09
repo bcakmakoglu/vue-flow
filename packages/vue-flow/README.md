9bb428f2bb0cb512ff333ae93167b61c954a7187
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-11-10 00:37:42 +0100

    chore: remove base `vue-flow` pkg

--- packages/vue-flow/README.md
+++
@@ -1,5 +0,0 @@
-# NOTICE
-
-- This package is supposed to be an all-inclusive pkg, containing all features available.
-- However, due to a name dispute, the pkg is not yet released.
-- Until the dispute is resolved, this pkg stays unreleased.
--- packages/vue-flow/src/index.ts
+++
@@ -1,9 +0,0 @@
-import '@vue-flow/core/dist/style.css'
-import '@vue-flow/core/dist/theme-default.css'
-import '@vue-flow/controls/dist/style.css'
-import '@vue-flow/minimap/dist/style.css'
-
-export * from '@vue-flow/background'
-export * from '@vue-flow/controls'
-export * from '@vue-flow/minimap'
-export * from '@vue-flow/core'
