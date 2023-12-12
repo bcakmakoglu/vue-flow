d556c2c76fbb20d39669e6dbd5ab94b6bf4e33a3
braks <78412429+bcakmakoglu@users.noreply.github.com>
2023-12-12 19:56:06 +0100

    chore(minimap): cleanup

--- packages/minimap/src/index.ts
+++ packages/minimap/src/index.ts
@@ -1,6 +1,6 @@
 import './style.css'
 
 export { default as MiniMap } from './MiniMap.vue'
-export { default as MiniMapNode } from './MiniMapNode'
+export { default as MiniMapNode } from './MiniMapNode.vue'
 
 export * from './types'
