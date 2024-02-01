cf770a9a58d6579a596818e649813987686db607
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-01 19:47:03 +0100

    refactor(core,composables): remove error from `useNodeId`

--- packages/core/src/composables/useNodeId.ts
+++ packages/core/src/composables/useNodeId.ts
@@ -2,12 +2,5 @@ import { inject } from 'vue'
 import { NodeId } from '../context'
 
 export function useNodeId() {
-  const nodeId = inject(NodeId, null)
-
-  if (!nodeId) {
-    // todo: emit error through hook
-    throw new Error('useNodeId must be called inside a Node component')
-  }
-
-  return nodeId
+  return inject(NodeId, null)
 }
