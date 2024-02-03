71a04daa29660d915700397404e690e4ebdde6d2
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-03 19:59:46 +0100

    chore(docs): update hidden example

--- docs/examples/hidden/App.vue
+++ docs/examples/hidden/App.vue
@@ -1,32 +1,31 @@
 <script setup>
-import { Background } from '@vue-flow/background'
-import { Panel, VueFlow, useVueFlow } from '@vue-flow/core'
 import { ref, watch } from 'vue'
+import { Background } from '@vue-flow/background'
+import { Panel, VueFlow } from '@vue-flow/core'
+
+const nodes = ref([
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+  { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
+  { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
+])
+
+const edges = ref([
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e1-3', source: '1', target: '3' },
+  { id: 'e3-4', source: '3', target: '4' },
+])
 
 const isHidden = ref(false)
 
-const { nodes, edges } = useVueFlow({
-  nodes: [
-    { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
-    { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
-    { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
-    { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
-  ],
-  edges: [
-    { id: 'e1-2', source: '1', target: '2' },
-    { id: 'e1-3', source: '1', target: '3' },
-    { id: 'e3-4', source: '3', target: '4' },
-  ],
-})
-
 watch(isHidden, () => {
-  nodes.value.forEach((n) => (n.hidden = isHidden.value))
-  edges.value.forEach((e) => (e.hidden = isHidden.value))
+  nodes.value = nodes.value.map((node) => ({ ...node, hidden: isHidden.value }))
+  edges.value = edges.value.map((edge) => ({ ...edge, hidden: isHidden.value }))
 })
 </script>
 
 <template>
-  <VueFlow fit-view-on-init>
+  <VueFlow :nodes="nodes" :edges="edges" fit-view-on-init>
     <Background />
 
     <Panel position="top-right">
