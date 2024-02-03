c8880c9c23025dfd18730ec873db4965cc38d115
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-03 18:42:33 +0100

    chore(docs): update custom node example

--- docs/examples/custom-node/ColorSelectorNode.vue
+++ docs/examples/custom-node/ColorSelectorNode.vue
@@ -1,60 +1,42 @@
 <script setup>
-import { Handle, Position, useVueFlow } from '@vue-flow/core'
+import { Handle, Position, useNodeId, useVueFlow } from '@vue-flow/core'
 import { colors } from './presets.js'
 
-const props = defineProps({
-  id: {
-    type: String,
-    required: true,
-  },
+defineProps({
   data: {
     type: Object,
     required: true,
   },
 })
 
+const nodeId = useNodeId()
+
 const { updateNodeData } = useVueFlow()
 
 function onSelect(color) {
-  updateNodeData(props.id, { color: color.value }, { replace: true })
+  updateNodeData(nodeId, { color }, { replace: true })
 }
 
 function onGradient() {
-  updateNodeData(props.id, { isGradient: true }, { replace: true })
+  updateNodeData(nodeId, { isGradient: true }, { replace: true })
 }
 </script>
 
 <template>
   <div>Select a color</div>
 
-  <div
-    class="color-selector"
-    style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: center; max-width: 90%; margin: auto; gap: 4px"
-  >
-    <template v-for="color of colors" :key="color.name">
-      <button :title="color.name" :style="{ backgroundColor: color.value }" type="button" @click="onSelect(color)" />
-    </template>
+  <div class="color-selector nodrag nopan">
+    <button
+      v-for="{ name: colorName, value: color } of colors"
+      :key="colorName"
+      :title="colorName"
+      :style="{ backgroundColor: color }"
+      type="button"
+      @click="onSelect(color)"
+    />
 
     <button class="animated-bg-gradient" title="gradient" type="button" @click="onGradient" />
   </div>
 
-  <!-- When using multiple handles of the same type, each handle should have a unique id -->
-  <Handle
-    id="a"
-    type="source"
-    :position="Position.Right"
-    :style="{ backgroundColor: data.color, filter: 'invert(100%)', top: '10px' }"
-  />
-
-  <Handle
-    id="b"
-    type="source"
-    :position="Position.Right"
-    :style="{
-      backgroundColor: data.color,
-      filter: 'invert(100%)',
-      bottom: '10px',
-      top: 'auto',
-    }"
-  />
+  <Handle id="a" type="source" :position="Position.Right" />
 </template>
