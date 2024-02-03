fc2f36b41958a9d0f4d105632984327f2f5c2835
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-03 16:51:23 +0100

    chore(docs): update custom node example

---
+++ docs/examples/custom-node/OutputNode.vue
@@ -0,0 +1,4 @@
+<script setup>
+const props = defineProps();
+
+const { }
--- docs/examples/custom-node/index.ts
+++ docs/examples/custom-node/index.ts
@@ -1,4 +1,5 @@
 export { default as CustomNodeApp } from './App.vue?raw'
-export { default as CustomNode } from './CustomNode.vue?raw'
+export { default as ColorSelectorNode } from './ColorSelectorNode.vue?raw'
+export { default as OutputNode } from './OutputNode.vue?raw'
 export { default as CustomNodeCSS } from './style.css?inline'
 export { default as ColorPresets } from './presets.js?raw'
