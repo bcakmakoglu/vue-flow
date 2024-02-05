49a9f4aeeef3ace8a20cf6d7d137565f919f7035
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-05 21:06:34 +0100

    chore(examples): cleanup tsconfig & vite config

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -2,28 +2,24 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
-import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
   resolve: {
     dedupe: ['vue'],
   },
   plugins: [
-    vue({
-      reactivityTransform: true,
-    }),
-    svgLoader(),
+    vue(),
     AutoImport({
-      imports: ['vue', '@vueuse/core', 'vue/macros'],
+      imports: ['vue', '@vueuse/core'],
       dts: resolve('src/auto-imports.d.ts'),
     }),
   ],
   server: {
     watch: {
-      ignored: ['!**/node_modules/@vue-flow/core/**'],
+      ignored: ['!**/node_modules/@vue-flow/**/*'],
     },
   },
   optimizeDeps: {
-    exclude: ['@vue-flow/core'],
+    exclude: ['@vue-flow/core', '@vue-flow/minimap', '@vue-flow/controls', '@vue-flow/background'],
   },
 })
