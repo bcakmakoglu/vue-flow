8af53d6c479ec52466a5415a4df93cf5c568ebb1
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-06 00:33:53 +0100

    chore(core): remove experimental from state

--- packages/core/src/types/flow.ts
+++ packages/core/src/types/flow.ts
@@ -197,10 +197,6 @@ export interface FlowProps {
 
   autoPanOnConnect?: boolean
   autoPanOnNodeDrag?: boolean
-
-  __experimentalFeatures?: {
-    nestedFlow?: boolean
-  }
 }
 
 // Todo: Remove in next major version
