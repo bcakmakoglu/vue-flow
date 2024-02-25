0f51e2ce24ff1a9be8500925f4fd330d6a62b6cb
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-02-25 10:23:54 +0100

    chore(docs): update custom node example

--- docs/examples/custom-node/OutputNode.vue
+++ docs/examples/custom-node/OutputNode.vue
@@ -5,10 +5,10 @@ const connections = useHandleConnections({
   type: 'target',
 })
 
-const data = useNodesData(() => connections.value[0]?.source)
+const nodesData = useNodesData(() => connections.value[0]?.source)
 </script>
 
 <template>
-  <Handle type="target" :position="Position.Left" :style="{ backgroundColor: data?.color, filter: 'invert(100%)' }" />
-  {{ data?.isGradient ? 'GRADIENT' : data?.color }}
+  <Handle type="target" :position="Position.Left" :style="{ backgroundColor: nodesData.data?.color, filter: 'invert(100%)' }" />
+  {{ nodesData.data?.isGradient ? 'GRADIENT' : nodesData.data?.color }}
 </template>
