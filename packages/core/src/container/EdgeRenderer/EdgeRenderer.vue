9c3c67c15fc1dd3c8702cf04ffd9a421aa3da556
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-06-05 19:52:22 +0200

    chore(core): cleanup

--- packages/core/src/container/EdgeRenderer/EdgeRenderer.vue
+++ packages/core/src/container/EdgeRenderer/EdgeRenderer.vue
@@ -1,66 +1,11 @@
 <script lang="ts" setup>
-import { getCurrentInstance, inject, resolveComponent } from 'vue'
 import EdgeWrapper from '../../components/Edges/EdgeWrapper'
 import ConnectionLine from '../../components/ConnectionLine'
-import type { EdgeComponent, EdgeUpdatable, GraphEdge } from '../../types'
-import { Slots } from '../../context'
 import { useVueFlow } from '../../composables'
-import { ErrorCode, VueFlowError, getEdgeZIndex } from '../../utils'
+import { getEdgeZIndex } from '../../utils'
 import MarkerDefinitions from './MarkerDefinitions.vue'
 
-const slots = inject(Slots)
-
-const {
-  edgesUpdatable,
-  edgesFocusable,
-  elementsSelectable,
-  findNode,
-  getEdges,
-  getEdgeTypes,
-  elevateEdgesOnSelect,
-  dimensions,
-  emits,
-} = useVueFlow()
-
-const instance = getCurrentInstance()
-
-function selectable(edgeSelectable?: boolean) {
-  return typeof edgeSelectable === 'undefined' ? elementsSelectable.value : edgeSelectable
-}
-function updatable(edgeUpdatable?: EdgeUpdatable) {
-  return typeof edgeUpdatable === 'undefined' ? edgesUpdatable.value : edgeUpdatable
-}
-function focusable(edgeFocusable?: boolean) {
-  return typeof edgeFocusable === 'undefined' ? edgesFocusable.value : edgeFocusable
-}
-
-function getType(type?: string, template?: GraphEdge['template']) {
-  const name = type || 'default'
-
-  const slot = slots?.[`edge-${name}`]
-  if (slot) {
-    return slot
-  }
-
-  let edgeType = template ?? getEdgeTypes.value[name]
-
-  if (typeof edgeType === 'string') {
-    if (instance) {
-      const components = Object.keys(instance.appContext.components)
-      if (components && components.includes(name)) {
-        edgeType = resolveComponent(name, false) as EdgeComponent
-      }
-    }
-  }
-
-  if (edgeType && typeof edgeType !== 'string') {
-    return edgeType
-  }
-
-  emits.error(new VueFlowError(ErrorCode.EDGE_TYPE_MISSING, edgeType))
-
-  return false
-}
+const { findNode, getEdges, elevateEdgesOnSelect, dimensions } = useVueFlow()
 </script>
 
 <script lang="ts">
