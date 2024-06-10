1c821decc91b7e0410cfc6fcdd462500969cb62d
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-06-10 22:58:11 +0200

    chore(docs): update transition example styles

--- docs/examples/transition/App.vue
+++ docs/examples/transition/App.vue
@@ -1,38 +1,47 @@
 <script setup>
 import { ref } from 'vue'
 import { Position, VueFlow, useVueFlow } from '@vue-flow/core'
+import { Background } from '@vue-flow/background'
 import TransitionEdge from './TransitionEdge.vue'
+
+const { onInit } = useVueFlow()
 
 const nodes = ref([
   {
     id: '1',
     type: 'input',
-    label: 'Dblclick me',
+    data: { label: 'DblClick me' },
     position: { x: 0, y: 0 },
     sourcePosition: Position.Right,
   },
   {
     id: '2',
     type: 'output',
-    label: 'Dblclick me',
+    data: { label: 'DblClick me' },
     position: { x: 1000, y: 1000 },
     targetPosition: Position.Left,
   },
 ])
 
-const edges = ref([{ id: 'e1-2', type: 'custom', source: '1', target: '2', animated: true, style: { stroke: '#fff' } }])
+const edges = ref([{ id: 'e1-2', type: 'custom', source: '1', target: '2', style: { stroke: '#fff' } }])
 
-const { onPaneReady } = useVueFlow()
-
-onPaneReady(({ fitView }) => {
+onInit(({ fitView }) => {
   fitView({ nodes: ['1'] })
 })
 </script>
 
 <template>
-  <VueFlow :nodes="nodes" :edges="edges" :style="{ backgroundColor: '#1A192B' }">
+  <VueFlow :nodes="nodes" :edges="edges" class="transition-flow">
+    <Background />
+
     <template #edge-custom="props">
       <TransitionEdge v-bind="props" />
     </template>
   </VueFlow>
 </template>
+
+<style>
+.transition-flow {
+  background-color: #1a192b;
+}
+</style>
