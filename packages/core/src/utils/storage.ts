willem-code/vue-flow-gh

chore(core): remove unused arg

packages/core/src/utils/storage.ts
@@ -62,7 +62,7 @@ export class Storage {
 
     const getters = useGetters(reactiveState, nodesMap, edgesMap)
 
-    const actions = useActions(id, reactiveState, nodesMap, edgesMap)
+    const actions = useActions(reactiveState, nodesMap, edgesMap)
 
     actions.setState({ ...reactiveState, ...preloadedState })
 

bcakmakoglu/vue-flow

packages/core/src/utils/storage.ts
@@ -78,7 +78,7 @@ export class Storage {
 
     const getters = useGetters(reactiveState, nodeLookup, edgeLookup)
 
-    const actions = useActions(id, reactiveState, nodeLookup, edgeLookup)
+    const actions = useActions(reactiveState, nodeLookup, edgeLookup)
 
     actions.setState({ ...reactiveState, ...preloadedState })
 