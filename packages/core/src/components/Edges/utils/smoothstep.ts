659ca6a0b74b7a96434d2e03a39a72154e5b4756
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-06-14 10:12:54 +0200

    feat(core): add EdgePathParams type to edge utils (#1479)
    * feat(core): add `EdgePathParams` type to edge utils
    
    * chore(changeset): add
    
    * chore(core): cleanup jsdocs

--- packages/core/src/components/Edges/utils/smoothstep.ts
+++ packages/core/src/components/Edges/utils/smoothstep.ts
@@ -1,5 +1,6 @@
 import type { XYPosition } from '../../../types'
 import { Position } from '../../../types'
+import type { EdgePathParams } from './general'
 import { getSimpleEdgeCenter } from './general'
 
 export interface GetSmoothStepPathParams {
@@ -196,27 +197,32 @@ function getBend(a: XYPosition, b: XYPosition, c: XYPosition, size: number): str
 
 /**
  * Get a smooth step path from source to target handle
- * @param params
- * @param params.sourceX - The x position of the source handle
- * @param params.sourceY - The y position of the source handle
- * @param params.sourcePosition - The position of the source handle (default: Position.Bottom)
- * @param params.targetX - The x position of the target handle
- * @param params.targetY - The y position of the target handle
- * @param params.targetPosition - The position of the target handle (default: Position.Top)
+ * @public
+ *
+ * @param smoothStepPathParams
+ * @param smoothStepPathParams.sourceX - The x position of the source handle
+ * @param smoothStepPathParams.sourceY - The y position of the source handle
+ * @param smoothStepPathParams.sourcePosition - The position of the source handle (default: Position.Bottom)
+ * @param smoothStepPathParams.targetX - The x position of the target handle
+ * @param smoothStepPathParams.targetY - The y position of the target handle
+ * @param smoothStepPathParams.targetPosition - The position of the target handle (default: Position.Top)
+ * @param smoothStepPathParams.borderRadius - The border radius of the edge (default: 5)
  * @returns A path string you can use in an SVG, the labelX and labelY position (center of path) and offsetX, offsetY between source handle and label
  */
-export function getSmoothStepPath({
-  sourceX,
-  sourceY,
-  sourcePosition = Position.Bottom,
-  targetX,
-  targetY,
-  targetPosition = Position.Top,
-  borderRadius = 5,
-  centerX,
-  centerY,
-  offset = 20,
-}: GetSmoothStepPathParams): [path: string, labelX: number, labelY: number, offsetX: number, offsetY: number] {
+export function getSmoothStepPath(smoothStepPathParams: GetSmoothStepPathParams): EdgePathParams {
+  const {
+    sourceX,
+    sourceY,
+    sourcePosition = Position.Bottom,
+    targetX,
+    targetY,
+    targetPosition = Position.Top,
+    borderRadius = 5,
+    centerX,
+    centerY,
+    offset = 20,
+  } = smoothStepPathParams
+
   const [points, labelX, labelY, offsetX, offsetY] = getPoints({
     source: { x: sourceX, y: sourceY },
     sourcePosition,
