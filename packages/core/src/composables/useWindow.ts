ea75f8aac0cbc2cfe962c460f435a834a8c8a5b7
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-06-19 18:17:15 +0200

    refactor(core): allow passing target to `useKeyPress`

--- packages/core/src/composables/useWindow.ts
+++ packages/core/src/composables/useWindow.ts
@@ -8,12 +8,12 @@ type UseWindow = Window & typeof globalThis & { chrome?: any }
 export function useWindow(): UseWindow {
   if (typeof window !== 'undefined') {
     return window as UseWindow
-  } else {
-    return {
-      chrome: false,
-      addEventListener(..._: Parameters<Window['addEventListener']>) {
-        // do nothing
-      },
-    } as UseWindow
   }
+
+  return {
+    chrome: false,
+    addEventListener(..._: Parameters<Window['addEventListener']>) {
+      // do nothing
+    },
+  } as UseWindow
 }
