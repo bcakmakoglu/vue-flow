6adcb657b9c5bb6c7c21677b62bd0a962a801fff
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-06-19 18:21:10 +0200

    feat(core): make `useKeyPress` public and export it

--- packages/core/src/composables/useKeyPress.ts
+++ packages/core/src/composables/useKeyPress.ts
@@ -74,7 +74,7 @@ const window = useWindow()
 /**
  * Composable that returns a boolean value if a key is pressed
  *
- * @internal
+ * @public
  * @param keyFilter - Can be a boolean, a string, an array of strings or a function that returns a boolean. If it's a boolean, it will act as if the key is always pressed. If it's a string, it will return true if a key matching that string is pressed. If it's an array of strings, it will return true if any of the strings match a key being pressed, or a combination (e.g. ['ctrl+a', 'ctrl+b'])
  * @param options - Options object
  */
