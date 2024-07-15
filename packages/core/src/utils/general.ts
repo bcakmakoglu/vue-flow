9ec7dc6c112d9acc89d1c1fe1bb4dfc901d18610
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-07-16 00:38:57 +0200

    refactor(core): remove `isMacOS`
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/utils/general.ts
+++ packages/core/src/utils/general.ts
@@ -1,7 +1,5 @@
 import type { GraphNode } from '../types'
 
-export const isMacOs = () => typeof navigator !== 'undefined' && navigator?.userAgent?.indexOf('Mac') >= 0
-
 export function getNodeDimensions(node: GraphNode): { width: number; height: number } {
   return {
     width: node.dimensions?.width ?? node.width ?? 0,
