5eeed505938fd8574e3319d63e31f339b4c28cf8
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-07-27 12:28:58 +0200

    refactor(node-toolbar): replace `ViewportTransform` type
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/node-toolbar/src/NodeToolbar.vue
+++ packages/node-toolbar/src/NodeToolbar.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { computed, inject, toRef } from 'vue'
-import type { GraphNode, Rect, ViewportTransform } from '@vue-flow/core'
+import type { Viewport } from '@xyflow/system'
+import type { GraphNode, Rect } from '@vue-flow/core'
 import { NodeIdInjection, Position, getRectOfNodes, useVueFlow } from '@vue-flow/core'
 
 import type { CSSProperties } from 'vue'
@@ -46,7 +47,7 @@ const wrapperStyle = computed<CSSProperties>(() => ({
   zIndex: zIndex.value,
 }))
 
-function getTransform(nodeRect: Rect, transform: ViewportTransform, position: Position, offset: number, align: Align): string {
+function getTransform(nodeRect: Rect, viewport: Viewport, position: Position, offset: number, align: Align): string {
   let alignmentOffset = 0.5
 
   if (align === 'start') {
@@ -58,8 +59,8 @@ function getTransform(nodeRect: Rect, transform: ViewportTransform, position: Po
   // position === Position.Top
   // we set the x any y position of the toolbar based on the nodes position
   let pos = [
-    (nodeRect.x + nodeRect.width * alignmentOffset) * transform.zoom + transform.x,
-    nodeRect.y * transform.zoom + transform.y - offset,
+    (nodeRect.x + nodeRect.width * alignmentOffset) * viewport.zoom + viewport.x,
+    nodeRect.y * viewport.zoom + viewport.y - offset,
   ]
   // and then shift it based on the alignment. The shift values are in %.
   let shift = [-100 * alignmentOffset, -100]
@@ -67,19 +68,19 @@ function getTransform(nodeRect: Rect, transform: ViewportTransform, position: Po
   switch (position) {
     case Position.Right:
       pos = [
-        (nodeRect.x + nodeRect.width) * transform.zoom + transform.x + offset,
-        (nodeRect.y + nodeRect.height * alignmentOffset) * transform.zoom + transform.y,
+        (nodeRect.x + nodeRect.width) * viewport.zoom + viewport.x + offset,
+        (nodeRect.y + nodeRect.height * alignmentOffset) * viewport.zoom + viewport.y,
       ]
       shift = [0, -100 * alignmentOffset]
       break
     case Position.Bottom:
-      pos[1] = (nodeRect.y + nodeRect.height) * transform.zoom + transform.y + offset
+      pos[1] = (nodeRect.y + nodeRect.height) * viewport.zoom + viewport.y + offset
       shift[1] = 0
       break
     case Position.Left:
       pos = [
-        nodeRect.x * transform.zoom + transform.x - offset,
-        (nodeRect.y + nodeRect.height * alignmentOffset) * transform.zoom + transform.y,
+        nodeRect.x * viewport.zoom + viewport.x - offset,
+        (nodeRect.y + nodeRect.height * alignmentOffset) * viewport.zoom + viewport.y,
       ]
       shift = [-100, -100 * alignmentOffset]
       break
