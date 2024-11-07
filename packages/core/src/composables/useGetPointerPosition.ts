b6c500dca8a8baa054802a80772990c723f83e2d
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-11-07 14:02:42 +0300

    fix(core): check if dragEnd event is UseDrag or MouseTouch event (#1680)
    * feat(core): add snapPosition util
    
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>
    
    * fix(core): check if dragEnd event is actually a UseDrag or MouseTouch event
    
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>
    
    * chore(core): cleanup
    
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>
    
    * chore(changeset): add
    
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/composables/useGetPointerPosition.ts
+++ packages/core/src/composables/useGetPointerPosition.ts
@@ -1,5 +1,7 @@
-import type { UseDragEvent } from './useDrag'
+import { getEventPosition, isUseDragEvent, pointToRendererPoint, snapPosition } from '../utils'
+import type { MouseTouchEvent } from '../types'
 import { useVueFlow } from './useVueFlow'
+import type { UseDragEvent } from './useDrag'
 
 /**
  * Composable that returns a function to get the pointer position
@@ -10,19 +12,17 @@ export function useGetPointerPosition() {
   const { viewport, snapGrid, snapToGrid } = useVueFlow()
 
   // returns the pointer position projected to the VF coordinate system
-  return ({ sourceEvent }: UseDragEvent) => {
-    const x = sourceEvent.touches ? sourceEvent.touches[0].clientX : sourceEvent.clientX
-    const y = sourceEvent.touches ? sourceEvent.touches[0].clientY : sourceEvent.clientY
+  return (event: UseDragEvent | MouseTouchEvent) => {
+    const evt = isUseDragEvent(event) ? event.sourceEvent : event
 
-    const pointerPos = {
-      x: (x - viewport.value.x) / viewport.value.zoom,
-      y: (y - viewport.value.y) / viewport.value.zoom,
-    }
+    const { x, y } = getEventPosition(evt)
+    const pointerPos = pointToRendererPoint({ x, y }, viewport.value)
+    const { x: xSnapped, y: ySnapped } = snapToGrid.value ? snapPosition(pointerPos, snapGrid.value) : pointerPos
 
     // we need the snapped position in order to be able to skip unnecessary drag events
     return {
-      xSnapped: snapToGrid.value ? snapGrid.value[0] * Math.round(pointerPos.x / snapGrid.value[0]) : pointerPos.x,
-      ySnapped: snapToGrid.value ? snapGrid.value[1] * Math.round(pointerPos.y / snapGrid.value[1]) : pointerPos.y,
+      xSnapped,
+      ySnapped,
       ...pointerPos,
     }
   }
