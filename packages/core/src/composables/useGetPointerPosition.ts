8cad9fd4f974352a232325fec36b6029ba5cf078
braks <78412429+bcakmakoglu@users.noreply.github.com>
2024-11-07 13:19:25 +0300

    chore(core): cleanup
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/composables/useGetPointerPosition.ts
+++ packages/core/src/composables/useGetPointerPosition.ts
@@ -1,5 +1,7 @@
-import { getEventPosition, pointToRendererPoint, snapPosition } from '../utils'
+import { getEventPosition, isUseDragEvent, pointToRendererPoint, snapPosition } from '../utils'
+import type { MouseTouchEvent } from '../types'
 import { useVueFlow } from './useVueFlow'
+import type { UseDragEvent } from './useDrag'
 
 /**
  * Composable that returns a function to get the pointer position
@@ -10,8 +12,10 @@ export function useGetPointerPosition() {
   const { viewport, snapGrid, snapToGrid } = useVueFlow()
 
   // returns the pointer position projected to the VF coordinate system
-  return (event: MouseEvent | TouchEvent) => {
-    const { x, y } = getEventPosition(event)
+  return (event: UseDragEvent | MouseTouchEvent) => {
+    const evt = isUseDragEvent(event) ? event.sourceEvent : event
+
+    const { x, y } = getEventPosition(evt)
     const pointerPos = pointToRendererPoint({ x, y }, viewport.value)
     const { x: xSnapped, y: ySnapped } = snapToGrid.value ? snapPosition(pointerPos, snapGrid.value) : pointerPos
 
