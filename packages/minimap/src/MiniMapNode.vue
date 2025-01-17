f04a77ce1b85b55a9046361562a6ff9a26ca0c73
braks <78412429+bcakmakoglu@users.noreply.github.com>
2025-01-17 16:17:42 +0100

    fix(minimap): bind attrs to minimap node
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/minimap/src/MiniMapNode.vue
+++ packages/minimap/src/MiniMapNode.vue
@@ -39,16 +39,22 @@ function onMouseLeave(event: MouseEvent) {
 export default {
   name: 'MiniMapNode',
   compatConfig: { MODE: 3 },
+  inheritAttrs: false,
 }
 </script>
 
 <template>
   <template v-if="!hidden && dimensions.width !== 0 && dimensions.height !== 0">
-    <component :is="miniMapSlots[`node-${props.type}`]" v-if="miniMapSlots[`node-${props.type}`]" v-bind="props" />
+    <component
+      :is="miniMapSlots[`node-${props.type}`]"
+      v-if="miniMapSlots[`node-${props.type}`]"
+      v-bind="{ ...props, ...$attrs }"
+    />
 
     <rect
       v-else
       :id="id"
+      v-bind="$attrs"
       class="vue-flow__minimap-node"
       :class="{ selected, dragging }"
       :x="position.x"
