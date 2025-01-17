67b652e159b7866eb2edb75d1324614ff7ca5702
braks <78412429+bcakmakoglu@users.noreply.github.com>
2025-01-17 16:38:04 +0100

    chore(examples): cleanup
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- examples/vite/src/Layouting/initial-elements.ts
+++ examples/vite/src/Layouting/initial-elements.ts
@@ -75,14 +75,14 @@ export const initialNodes: ProcessNode[] = [
 ]
 
 export const initialEdges: ProcessEdge[] = [
-  { id: 'e1-2', source: '1', target: '2', type, animated: true },
-  { id: 'e1-3', source: '1', target: '3', type, animated: true },
-  { id: 'e2-2a', source: '2', target: '2a', type, animated: true },
-  { id: 'e2-2b', source: '2', target: '2b', type, animated: true },
-  { id: 'e2-2c', source: '2', target: '2c', type, animated: true },
-  { id: 'e2c-2d', source: '2c', target: '2d', type, animated: true },
-  { id: 'e3-7', source: '3', target: '4', type, animated: true },
-  { id: 'e4-5', source: '4', target: '5', type, animated: true },
-  { id: 'e5-6', source: '5', target: '6', type, animated: true },
-  { id: 'e5-7', source: '5', target: '7', type, animated: true },
+  { id: 'e1-2', source: '1', target: '2', type },
+  { id: 'e1-3', source: '1', target: '3', type },
+  { id: 'e2-2a', source: '2', target: '2a', type },
+  { id: 'e2-2b', source: '2', target: '2b', type },
+  { id: 'e2-2c', source: '2', target: '2c', type },
+  { id: 'e2c-2d', source: '2c', target: '2d', type },
+  { id: 'e3-7', source: '3', target: '4', type },
+  { id: 'e4-5', source: '4', target: '5', type },
+  { id: 'e5-6', source: '5', target: '6', type },
+  { id: 'e5-7', source: '5', target: '7', type },
 ]
