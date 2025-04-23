88c927e9bbc6520b5706dcaddba07c5ec395b215
braks <78412429+bcakmakoglu@users.noreply.github.com>
2025-04-23 19:39:33 +0200

    chore(core): cleanup getHandleBounds
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/utils/node.ts
+++ packages/core/src/utils/node.ts
@@ -8,22 +8,25 @@ export function getHandleBounds(
   nodeElement: HTMLDivElement,
   nodeBounds: DOMRect,
   zoom: number,
-): HandleElement[] {
+  nodeId: string,
+): HandleElement[] | null {
   const handles = nodeElement.querySelectorAll(`.vue-flow__handle.${type}`)
 
-  const handlesArray = Array.from(handles) as HTMLDivElement[]
+  if (!handles?.length) {
+    return null
+  }
 
-  return handlesArray.map((handle): HandleElement => {
+  return Array.from(handles).map((handle): HandleElement => {
     const handleBounds = handle.getBoundingClientRect()
 
     return {
       id: handle.getAttribute('data-handleid'),
-      position: handle.getAttribute('data-handlepos') as unknown as Position,
-      nodeId: handle.getAttribute('data-nodeid') as string,
       type,
+      nodeId,
+      position: handle.getAttribute('data-handlepos') as unknown as Position,
       x: (handleBounds.left - nodeBounds.left) / zoom,
       y: (handleBounds.top - nodeBounds.top) / zoom,
-      ...getDimensions(handle),
+      ...getDimensions(handle as HTMLDivElement),
     }
   })
 }
