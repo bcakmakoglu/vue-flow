62a1d41a4336fa3596f1f82cfdcabfffdbd00c65
braks <78412429+bcakmakoglu@users.noreply.github.com>
2025-10-08 18:28:38 +0200

    chore(docs): update showcase
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- docs/components/home/Showcase.vue
+++ docs/components/home/Showcase.vue
@@ -7,7 +7,7 @@
   >
     <div class="max-w-5/6 2xl:max-w-7xl mx-auto py-12 md:py-16">
       <div class="flex flex-col items-center justify-center gap-2">
-        <h1>Putting Ideas into Action: The Showcase</h1>
+        <h1 class="text-center">Putting Ideas into Action - The Showcase</h1>
 
         <p class="font-medium max-w-3/4 text-center">
           Whether you're a seasoned developer or just starting your journey with Vue Flow, this showcase offers inspiration and
@@ -15,22 +15,23 @@
         </p>
 
         <div
-          class="divide-y divide-secondary md:divide-y-0 flex flex-col md:flex-row justify-between items-center gap-12 mt-8 text-center md:text-left"
+          class="divide-y divide-secondary md:divide-y-0 flex flex-col md:flex-row justify-between items-start gap-12 mt-8 text-center md:text-left"
         >
           <div class="flex-1 flex flex-col items-center md:items-start">
             <div
               class="h-[200px] md:min-h-[300px] w-full shadow-xl rounded-xl font-mono uppercase border-1 border-gray overflow-hidden"
             >
-              <img class="object-cover h-full w-full" src="/assets/octai.png" alt="Octai" />
+              <img class="object-cover h-full w-full" src="/assets/n8n.webp" alt="n8n" />
             </div>
 
             <div class="px-1">
-              <a href="https://octai.com" target="_blank">
-                <h2 class="text-3xl mt-6 font-semibold inline-block">Octai</h2>
+              <a href="https://n8n.io" target="_blank">
+                <h2 class="text-3xl mt-6 font-semibold inline-block">n8n</h2>
               </a>
 
               <h3 class="text-lg">
-                A Machine-Learning NoCode platform using a node-based UI for Data Scientists and Developers.
+                n8n is an open-source workflow automation platform that lets you connect apps, APIs, and services to create
+                automated processes using a visual, node-based interface.
               </h3>
             </div>
           </div>
@@ -47,7 +48,10 @@
                 <h2 class="text-3xl mt-6 font-semibold inline-block">Kestra</h2>
               </a>
 
-              <h3 class="text-lg">An open-source orchestrator that makes both scheduled and event-driven workflows easy.</h3>
+              <h3 class="text-lg">
+                Kestra is an open-source, declarative orchestration and workflow automation platform that enables you to define,
+                schedule, and govern complex processes as code (YAML) with UI support.
+              </h3>
             </div>
           </div>
         </div>
