8ba6217a78292b42062e951c4ee0a195f749052a
braks <78412429+bcakmakoglu@users.noreply.github.com>
2026-05-14 17:11:56 +0200

    refactor(core): use getNodeToolbarTransform from system
    Signed-off-by: braks <78412429+bcakmakoglu@users.noreply.github.com>

--- packages/core/src/components/NodeToolbar/NodeToolbar.vue
+++ packages/core/src/components/NodeToolbar/NodeToolbar.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 import { computed, inject } from 'vue'
-import type { Viewport } from '@xyflow/system'
+import { getNodeToolbarTransform } from '@xyflow/system'
 import type { CSSProperties } from 'vue'
 import { useVueFlow } from '../../composables'
 import { NodeId } from '../../context'
-import type { GraphNode, Rect } from '../../types'
+import type { GraphNode } from '../../types'
 import { Position } from '../../types'
 import { getNodesBounds } from '../../utils'
 
-import type { Align, NodeToolbarProps } from './types'
+import type { NodeToolbarProps } from './types'
 
 const props = withDefaults(defineProps<NodeToolbarProps>(), {
   position: Position.Top,
@@ -47,51 +47,9 @@ const zIndex = computed(() => Math.max(...nodes.value.map((node) => (node.intern
 
 const wrapperStyle = computed<CSSProperties>(() => ({
   position: 'absolute',
-  transform: getTransform(nodeRect.value, viewport.value, props.position, props.offset, props.align),
+  transform: getNodeToolbarTransform(nodeRect.value, viewport.value, props.position, props.offset, props.align),
   zIndex: zIndex.value,
 }))
-
-function getTransform(nodeRect: Rect, viewport: Viewport, position: Position, offset: number, align: Align): string {
-  let alignmentOffset = 0.5
-
-  if (align === 'start') {
-    alignmentOffset = 0
-  } else if (align === 'end') {
-    alignmentOffset = 1
-  }
-
-  // position === Position.Top
-  // we set the x any y position of the toolbar based on the nodes position
-  let pos = [
-    (nodeRect.x + nodeRect.width * alignmentOffset) * viewport.zoom + viewport.x,
-    nodeRect.y * viewport.zoom + viewport.y - offset,
-  ]
-  // and then shift it based on the alignment. The shift values are in %.
-  let shift = [-100 * alignmentOffset, -100]
-
-  switch (position) {
-    case Position.Right:
-      pos = [
-        (nodeRect.x + nodeRect.width) * viewport.zoom + viewport.x + offset,
-        (nodeRect.y + nodeRect.height * alignmentOffset) * viewport.zoom + viewport.y,
-      ]
-      shift = [0, -100 * alignmentOffset]
-      break
-    case Position.Bottom:
-      pos[1] = (nodeRect.y + nodeRect.height) * viewport.zoom + viewport.y + offset
-      shift[1] = 0
-      break
-    case Position.Left:
-      pos = [
-        nodeRect.x * viewport.zoom + viewport.x - offset,
-        (nodeRect.y + nodeRect.height * alignmentOffset) * viewport.zoom + viewport.y,
-      ]
-      shift = [-100, -100 * alignmentOffset]
-      break
-  }
-
-  return `translate(${pos[0]}px, ${pos[1]}px) translate(${shift[0]}%, ${shift[1]}%)`
-}
 </script>
 
 <script lang="ts">
